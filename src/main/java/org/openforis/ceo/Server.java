0c83b97c9377bba78b2755df52bc14989786f540
Billy Ashmall <billy.ashmall@nasa.gov>
2017-03-28 16:43:59 -0500

    Started to incorporate GEO-DASH
    This is the first portion of the GEO-DASH.  There is still work to be
    done.

--- src/main/java/org/openforis/ceo/Server.java
+++ src/main/java/org/openforis/ceo/Server.java
@@ -2,14 +2,27 @@ package org.openforis.ceo;
 
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.net.URL;
+import java.util.UUID;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
 import spark.servlet.SparkApplication;
 import static spark.Spark.get;
 import static spark.Spark.exception;
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
 import spark.template.freemarker.FreeMarkerEngine;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
 
 public class Server implements SparkApplication {
 
@@ -51,6 +64,73 @@ public class Server implements SparkApplication {
         get("/account",        Views.account,       renderer);
         get("/dashboard",      Views.dashboard,     renderer);
         get("/admin",          Views.admin,         renderer);
+        get("/geo-dash",          Views.geodash,         renderer);
+        get("/geo-dash/id/:id", (req, res) -> {
+            boolean isAdmin = true; //Need to link to user and check
+            String returnString = "";
+            JSONParser projectParser = new JSONParser();
+            Object projObj = projectParser.parse(new FileReader("proj.json"));
+            JSONArray jsonProjArray = (JSONArray)projObj;
+            boolean found = false;
+            for (int i = 0; i< jsonProjArray.size(); i++) {
+                JSONParser parser2 = new JSONParser();
+
+                Object obj = parser2.parse(jsonProjArray.get(i).toString());
+                JSONObject jsonObject = (JSONObject)obj;
+                String theID = req.params(":id").toString();
+                String jsonPID = jsonObject.get("projectID").toString();
+                if(theID.equals(jsonPID))
+                {
+                    Object dashboardObj = parser2.parse(new FileReader("dash-" + jsonObject.get("dashboard").toString() +".json"));
+                    JSONObject jsonObject2 = (JSONObject)dashboardObj;
+                    returnString = jsonObject2.toJSONString();  //jsonObject.get("dashboard").toString()
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found)
+            {
+                if(isAdmin) {
+                    //add new proj to json file and create json file of that name
+                    String newUUIDString = UUID.randomUUID().toString();
+                    JSONObject newitem = new JSONObject();
+                    newitem.put("projectID", req.params("id").toString());
+                    newitem.put("dashboard", newUUIDString);
+                    jsonProjArray.add(newitem);
+
+
+                    try (FileWriter file = new FileWriter("proj.json")) {
+                        file.write(jsonProjArray.toJSONString());
+                    }
+
+
+                    //need to create dash-UUID.json file with all info passed
+                    // porjectID, projectTitle, widgets: [], dashboardID
+                    String pTitle = req.queryParams("title").toString();
+
+
+                    String newDashboard = "{\"projectID\":" + req.params("id").toString() + ",\"projectTitle\":\"" + req.queryParams("title").toString() + "\",\"widgets\":[], \"dashboardID\":\"" + newUUIDString + "\"}";//JSONValue.parse("{\"projectID\":" + req.params("id").toString() + ",\"projectTitle\":" + req.queryParams("title").toString() + ",\"widgets\":null, \"dashboardID\":"+ newUUIDString +"}")
+                    //var jsonObject2 = newDashboard as JSONObject
+                    returnString = newDashboard; //jsonObject2.toJSONString()
+                    new FileWriter("dash-" + newUUIDString + ".json").write(returnString);
+                    try (FileWriter file = new FileWriter("dash-" + newUUIDString + ".json")) {
+                        file.write(returnString);
+                    }
+                }
+
+                //returnString = newUUIDString
+            }
+
+
+            if(req.queryParams("callback") != null){
+                returnString = req.queryParams("callback").toString() + "(" + returnString + ")";
+            }
+
+
+                return returnString ;
+
+        });
         get("*",               Views.pageNotFound,  renderer);
 
         // Handle Exceptions
@@ -60,7 +140,7 @@ public class Server implements SparkApplication {
     // Maven/Gradle entry point for running with embedded Jetty webserver
     public static void main(String[] args) {
         // Set the webserver port
-        port(8080);
+        port(8081);
 
         // Set up the routing table
         declareRoutes();
