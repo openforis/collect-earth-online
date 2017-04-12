0f3368696b04ca6a5fdf87d01621cdc42b4dbb02
Gary Johnson <gwjohnso@uvm.edu>
2017-04-11 20:01:40 -0400

    Rewrote AJAX.geodashId in functional style and fixed several bugs in the original implementation.

--- src/main/java/org/openforis/ceo/AJAX.java
+++ src/main/java/org/openforis/ceo/AJAX.java
@@ -2,10 +2,10 @@ package org.openforis.ceo;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Optional;
 import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import spark.Request;
 import spark.Response;
@@ -17,78 +17,67 @@ public class AJAX {
     }
 
     public static String geodashId(Request req, Response res) {
-        JSONArray jsonProjArray;
-        try {
-            jsonProjArraynew JSONParser().parse(new FileReader("proj.json"));
+        try (FileReader projectFileReader = new FileReader("proj.json")) {
+            JSONParser parser = new JSONParser();
+            JSONArray projects = (JSONArray) parser.parse(projectFileReader);
+
+            Optional matchingProject = projects
+                .stream()
+                .filter(project -> req.params(":id").equals(((JSONObject) project).get("projectID")))
+                .map(project -> {
+                        try (FileReader dashboardFileReader = new FileReader("dash-" + ((JSONObject) project).get("dashboard") + ".json")) {
+                            return (JSONObject) parser.parse(dashboardFileReader);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                .findFirst();
+
+            if (matchingProject.isPresent()) {
+                if (req.queryParams("callback") != null) {
+                    return req.queryParams("callback") + "(" + matchingProject.get().toString() + ")";
+                } else {
+                    return matchingProject.get().toString();
+                }
+            } else {
+                if (true) { // FIXME: Make sure this is true if the user has role admin
+                    String newUUID = UUID.randomUUID().toString();
+
+                    JSONObject newProject = new JSONObject();
+                    newProject.put("projectID", req.params(":id"));
+                    newProject.put("dashboard", newUUID);
+                    projects.add(newProject);
+
+                    try (FileWriter projectFileWriter = new FileWriter("proj.json")) {
+                        projectFileWriter.write(projects.toString());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    JSONObject newDashboard = new JSONObject();
+                    newDashboard.put("projectID", req.params(":id"));
+                    newDashboard.put("projectTitle", req.queryParams("title"));
+                    newDashboard.put("widgets", "[]");
+                    newDashboard.put("dashboardID", newUUID);
+
+                    try (FileWriter dashboardFileWriter = new FileWriter("dash-" + newUUID + ".json")) {
+                        dashboardFileWriter.write(newDashboard.toString());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    if (req.queryParams("callback") != null) {
+                        return req.queryParams("callback") + "(" + newDashboard.toString() + ")";
+                    } else {
+                        return newDashboard.toString();
+                    }
+                } else {
+                    return "No project exists with ID: " + req.params(":id") + ".";
+                }
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        String returnString = "";
-        if (req.queryParams("callback") != null) {
-            returnString = req.queryParams("callback").toString() + "(" + returnString + ")";
-        }
-        return returnString;
-    }
-
-    public static String geodashId(Request req, Response res) {
-        boolean isAdmin = true; //Need to link to user and check
-        String returnString = "";
-        JSONParser projectParser = new JSONParser();
-        Object projObj = projectParser.parse(new FileReader("proj.json"));
-        JSONArray jsonProjArray = (JSONArray)projObj;
-        boolean found = false;
-
-        for (int i = 0; i < jsonProjArray.size(); i++) {
-            JSONParser parser2 = new JSONParser();
-            Object obj = parser2.parse(jsonProjArray.get(i).toString());
-            JSONObject jsonObject = (JSONObject)obj;
-            String theID = req.params(":id").toString();
-            String jsonPID = jsonObject.get("projectID").toString();
-            if (theID.equals(jsonPID)) {
-                Object dashboardObj = parser2.parse(new FileReader("dash-" + jsonObject.get("dashboard").toString() +".json"));
-                JSONObject jsonObject2 = (JSONObject)dashboardObj;
-                returnString = jsonObject2.toJSONString();  //jsonObject.get("dashboard").toString()
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            if (isAdmin) {
-                //add new proj to json file and create json file of that name
-                String newUUIDString = UUID.randomUUID().toString();
-                JSONObject newitem = new JSONObject();
-                newitem.put("projectID", req.params("id").toString());
-                newitem.put("dashboard", newUUIDString);
-                jsonProjArray.add(newitem);
-
-                try (FileWriter file = new FileWriter("proj.json")) {
-                    file.write(jsonProjArray.toJSONString());
-                }
-
-                //need to create dash-UUID.json file with all info passed
-                // porjectID, projectTitle, widgets: [], dashboardID
-                String pTitle = req.queryParams("title").toString();
-
-                String newDashboard = "{\"projectID\":" + req.params("id").toString() + ",\"projectTitle\":\"" + req.queryParams("title").toString() + "\",\"widgets\":[], \"dashboardID\":\"" + newUUIDString + "\"}";
-                // JSONValue.parse("{\"projectID\":" + req.params("id").toString() + ",\"projectTitle\":" + req.queryParams("title").toString() + ",\"widgets\":null, \"dashboardID\":"+ newUUIDString +"}")
-                //var jsonObject2 = newDashboard as JSONObject
-                returnString = newDashboard; //jsonObject2.toJSONString()
-                new FileWriter("dash-" + newUUIDString + ".json").write(returnString);
-                try (FileWriter file = new FileWriter("dash-" + newUUIDString + ".json")) {
-                    file.write(returnString);
-                }
-            }
-
-            //returnString = newUUIDString
-        }
-
-        if (req.queryParams("callback") != null) {
-            returnString = req.queryParams("callback").toString() + "(" + returnString + ")";
-        }
-
-        return returnString;
     }
 
 }
