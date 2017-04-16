b962965363893df754be2055d8a31accf11052b7
Gary Johnson <gwjohnso@uvm.edu>
2017-04-16 17:15:09 -0400

    Removed the $scope.project object from admin.js and correctly implemented submitForm().

--- src/main/resources/public/js/admin.js
+++ src/main/resources/public/js/admin.js
@@ -7,28 +7,25 @@
 var admin = {};
 
 admin.controller = function ($scope, $http) {
-    // FIXED: Set using an AJAX request 
     $scope.projectList = getProjectList($http);
-    $scope.project = {};
-    $scope.project.currentProjectId = "0";
+    $scope.currentProjectId = "0";
     $scope.currentProject = null;
-    $scope.project.projectName = "";
-    $scope.project.projectDescription = "";
-    $scope.project.numPlots = "";
-    $scope.project.plotRadius = "";
-    $scope.project.sampleType = "random";
-    $scope.project.samplesPerPlot = "";
-    $scope.project.sampleResolution = "";
-    $scope.project.lonMin = "";
-    $scope.project.latMin = "";
-    $scope.project.lonMax = "";
-    $scope.project.latMax = "";
-    $scope.project.currentImagery = "DigitalGlobeRecentImagery+Streets";
+    $scope.projectName = "";
+    $scope.projectDescription = "";
+    $scope.numPlots = "";
+    $scope.plotRadius = "";
+    $scope.sampleType = "random";
+    $scope.samplesPerPlot = "";
+    $scope.sampleResolution = "";
+    $scope.lonMin = "";
+    $scope.latMin = "";
+    $scope.lonMax = "";
+    $scope.latMax = "";
+    $scope.currentImagery = "DigitalGlobeRecentImagery+Streets";
     $scope.valueName = "";
     $scope.valueColor = "#000000";
     $scope.valueImage = "";
-    $scope.project.sampleValues = [];
-    $scope.project.isGridded = false;
+    $scope.sampleValues = [];
 
     // Initialize the base map and enable the dragbox interaction
     map_utils.digital_globe_base_map({div_name: "new-project-map",
@@ -50,9 +47,9 @@ admin.controller = function ($scope, $http) {
     $scope.deleteCurrentProject = function() {
         $http.get('archive-project').
             then (function() {
-                alert("Project \"" + $scope.project.projectName + "\" has been deleted." + "\n");
-                $scope.project.currentProjectId = 0;
-                getProjectList();            
+                alert("Project \"" + $scope.projectName + "\" has been deleted." + "\n");
+                $scope.currentProjectId = 0;
+                getProjectList();
             }, function(response) {
                 console.log(response.status);
             });
@@ -67,66 +64,65 @@ admin.controller = function ($scope, $http) {
     };
 
     $scope.setCurrentProject = function() {
-        var project = $scope.getProjectById($scope.project.currentProjectId);
+        var project = $scope.getProjectById($scope.currentProjectId);
         $scope.currentProject = project;
 
         if (project) {
-            // FIXED: Set using an AJAX request
-            var plotData = $scope.projectList.plot_data[$scope.project.currentProjectId];
-            $scope.project.projectName = project.name;
-            $scope.project.projectDescription = project.description;
-            $scope.project.numPlots = plotData.length;
-            $scope.project.plotRadius = plotData[0].plot.radius;
+            var plotData = $scope.projectList.plot_data[$scope.currentProjectId];
+            $scope.projectName = project.name;
+            $scope.projectDescription = project.description;
+            $scope.numPlots = plotData.length;
+            $scope.plotRadius = plotData[0].plot.radius;
             if (project.sample_resolution) {
-                $scope.project.sampleType = "gridded";
+                $scope.sampleType = "gridded";
                 document.getElementById("gridded-sample-type").checked = true;
                 utils.enable_element("samples-per-plot");
                 utils.enable_element("sample-resolution");
             } else {
-                $scope.project.sampleType = "random";
+                $scope.sampleType = "random";
                 document.getElementById("random-sample-type").checked = true;
                 utils.enable_element("samples-per-plot");
                 utils.disable_element("sample-resolution");
             }
-            $scope.project.samplesPerPlot = plotData[0].samples.length;
-            $scope.project.sampleResolution = project.sample_resolution || "";
+            $scope.samplesPerPlot = plotData[0].samples.length;
+            $scope.sampleResolution = project.sample_resolution || "";
             var boundaryExtent = map_utils.polygon_extent(project.boundary);
-            $scope.project.lonMin = boundaryExtent[0];
-            $scope.project.latMin = boundaryExtent[1];
-            $scope.project.lonMax = boundaryExtent[2];
-            $scope.project.latMax = boundaryExtent[3];
-            $scope.project.currentImagery = project.imagery;
-            map_utils.set_current_imagery($scope.project.currentImagery);
+            $scope.lonMin = boundaryExtent[0];
+            $scope.latMin = boundaryExtent[1];
+            $scope.lonMax = boundaryExtent[2];
+            $scope.latMax = boundaryExtent[3];
+            $scope.currentImagery = project.imagery;
+            map_utils.set_current_imagery($scope.currentImagery);
             map_utils.disable_dragbox_draw();
             map_utils.draw_polygon(project.boundary);
-            $scope.project.sampleValues = project.sample_values;
+            $scope.sampleValues = project.sample_values;
         } else {
-            $scope.project.projectName = "";
-            $scope.project.projectDescription = "";
-            $scope.project.numPlots = "";
-            $scope.project.plotRadius = "";
-            $scope.project.sampleType = "random";
+            $scope.projectName = "";
+            $scope.projectDescription = "";
+            $scope.numPlots = "";
+            $scope.plotRadius = "";
+            $scope.sampleType = "random";
             document.getElementById("random-sample-type").checked = true;
             utils.enable_element("samples-per-plot");
             utils.disable_element("sample-resolution");
-            $scope.project.samplesPerPlot = "";
-            $scope.project.sampleResolution = "";
-            $scope.project.lonMin = "";
-            $scope.project.latMin = "";
-            $scope.project.lonMax = "";
-            $scope.project.latMax = "";
-            $scope.project.currentImagery = "DigitalGlobeRecentImagery+Streets";
-            map_utils.set_current_imagery($scope.project.currentImagery);
+            $scope.samplesPerPlot = "";
+            $scope.sampleResolution = "";
+            $scope.lonMin = "";
+            $scope.latMin = "";
+            $scope.lonMax = "";
+            $scope.latMax = "";
+            $scope.currentImagery = "DigitalGlobeRecentImagery+Streets";
+            map_utils.set_current_imagery($scope.currentImagery);
             map_utils.enable_dragbox_draw();
             map_utils.map_ref.removeLayer(map_utils.current_boundary);
             map_utils.current_boundary = null;
             map_utils.zoom_and_recenter_map(102.0, 17.0, 5);
-            $scope.project.sampleValues = [];
+            $scope.sampleValues = [];
         }
     };
 
     $scope.exportCurrentPlotData = function () {
-        var project_id = parseInt($scope.project.currentProjectId);
+        var project_id = parseInt($scope.currentProjectId);
         if (project_id != 0) {
             $http.post("dump-project-aggregate-data",
                        {project_id: project_id})
@@ -136,31 +132,21 @@ admin.controller = function ($scope, $http) {
                     console.log(response);
                     alert("Error downloading data for this project. See the console for more information.");
                 });
-        } 
+        }
     };
 
     $scope.submitForm = function ($event) {
-        if ($scope.project.currentProjectId != "0") {
-           if (confirm("Do you REALLY want to delete this project?!")) {
-               $scope.deleteCurrentProject();
-           } 
-
-           var spinnerElem = document.getElementById("spinner");
-           spinnerElem.style.visibility = "visible";
-           $scope.project.buttonDelete = $event.currentTarget;
-           $scope.project.buttonDelete.value = "Processing...please wait...";
-           
-
-           postFormData($scope.project, $http).
-               then(function(response, data, status, headers) {
-                   spinnerElem.style.visibility = "hidden";
-                   response.config.data["buttonDelete"].value = "Delete this project";
-               }, function(response) { 
-                   console.log(response.status);
-               });
+        if ($scope.currentProjectId != "0") {
+            if (confirm("Do you REALLY want to delete this project?!")) {
+                $scope.deleteCurrentProject();
+            }
+        } else {
+            $event.currentTarget.value = "Processing...please wait...";
+            document.getElementById("spinner").style.visibility = "visible";
+            document.getElementById("project-management-form").submit();
         }
     };
-  
+
     $scope.setSampleType = function (sampleType) {
         if (sampleType == "random") {
             utils.enable_element("samples-per-plot");
@@ -172,11 +158,11 @@ admin.controller = function ($scope, $http) {
     };
 
     $scope.setCurrentImagery = function () {
-        map_utils.set_current_imagery($scope.project.currentImagery);
+        map_utils.set_current_imagery($scope.currentImagery);
     };
 
     $scope.removeSampleValueRow = function (sampleValueName) {
-        $scope.project.sampleValues = $scope.project.sampleValues.filter(
+        $scope.sampleValues = $scope.sampleValues.filter(
             function (sampleValue) {
                 return sampleValue.name != sampleValueName;
             }
@@ -201,22 +187,17 @@ admin.controller = function ($scope, $http) {
 };
 
 var getProjectList = function ($http) {
-//  FIXME:  GARY - Once the get-all-projects route is created, uncomment the block of code below
-/*    $http.get('get-all-projects').
-        then (function(data) {
-            return data;
-        }, function(response) {
-            console.log(response.status);
-            return {};
-        });
-*/
+    //  FIXME:  GARY - Once the get-all-projects route is created, uncomment the block of code below
+    /*    $http.get('get-all-projects').
+          then (function(data) {
+          return data;
+          }, function(response) {
+          console.log(response.status);
+          return {};
+          });
+    */
     return ceo_sample_data.project_list;
 }
-
-var postFormData = function (data, $http) {
-   return $http.post('admin', data);
-}
-
 
 angular
     .module('collectEarth')
