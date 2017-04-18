7f2c0c4bcbc5a0aaf483d6bee147cfaa894a80aa
Gary Johnson <gwjohnso@uvm.edu>
2017-04-18 11:44:47 -0400

    Reimplemented the Dashboard page's Angular code to correctly declare a module, controller, constructor function, and initialization code.

--- src/main/resources/public/js/dashboard.js
+++ src/main/resources/public/js/dashboard.js
@@ -1,55 +1,53 @@
-/*****************************************************************************
- ***
- *** Create the dashboard object to act as a namespace for this file
- ***
- *****************************************************************************/
+angular.module("dashboard", []).controller("DashboardController", ["$http", function DashboardController($http) {
+    this.projectList = [];
+    this.currentProjectId = "";
+    this.currentProject = null;
+    this.currentPlot = null;
+    this.currentSamples = [];
+    this.userSamples = {};
 
-var dashboard = {};
-
-dashboard.controller = function ($scope, $http) {
-    $scope.getProjectList = function () {
-        // FIXME: Replace with an AJAX request
+    // FIXME: Replace with an AJAX request and implement this endpoint
+    this.getProjectList = function () {
         // $http.get("get-all-projects")
         //     .then(function successCallback(response) {
         //         return response.data;
         //     }, function errorCallback(response) {
         //         console.log(response);
-        //         alert("Error retrieving the project list. See the console for more information.");
-        //         return {};
+        //         alert("Error retrieving the project list. See console for details.");
+        //         return [];
         //     });
         return ceo_sample_data.project_list;
     };
 
-    $scope.projectList = $scope.getProjectList();
-
-    $scope.getProjectById = function (projectId) {
-        $scope.projectList.find(
+    this.getProjectById = function (projectId) {
+        return this.projectList.find(
             function (project) {
                 return project.id == projectId;
             }
         );
     };
 
-    $scope.currentProjectId = document.getElementById("initial-project-id").value;
-    $scope.currentProject = $scope.getProjectById($scope.currentProjectId);
-    $scope.currentPlot = null;
-    $scope.currentSamples = [];
-    $scope.userSamples = {};
+    this.initialize = function () {
+        // Load the projectList and currentProject
+        this.projectList = this.getProjectList();
+        this.currentProjectId = document.getElementById("initial-project-id").value;
+        this.currentProject = this.getProjectById(parseInt(this.currentProjectId));
 
-    // Initialize the base map and show the selected project's boundary
-    map_utils.digital_globe_base_map({div_name: "image-analysis-pane",
-                                      center_coords: [102.0, 17.0],
-                                      zoom_level: 5});
-    map_utils.set_current_imagery($scope.currentProject.imagery);
-    map_utils.draw_polygon($scope.currentProject.boundary);
+        // Initialize the base map and show the selected project's boundary
+        map_utils.digital_globe_base_map({div_name: "image-analysis-pane",
+                                          center_coords: [102.0, 17.0],
+                                          zoom_level: 5});
+        map_utils.set_current_imagery(this.currentProject.imagery);
+        map_utils.draw_polygon(this.currentProject.boundary);
+    };
 
-    $scope.switchProject = function () {
-        var newProject = $scope.getProjectById($scope.currentProjectId);
+    this.switchProject = function () {
+        var newProject = this.getProjectById(parseInt(this.currentProjectId));
         if (newProject) {
-            $scope.currentProject = newProject;
-            $scope.currentPlot = null;
-            $scope.currentSamples = [];
-            $scope.userSamples = {};
+            this.currentProject = newProject;
+            this.currentPlot = null;
+            this.currentSamples = [];
+            this.userSamples = {};
             map_utils.remove_plot_layer();
             map_utils.remove_sample_layer();
             map_utils.disable_selection();
@@ -61,33 +59,33 @@ dashboard.controller = function ($scope, $http) {
         }
     };
 
-    $scope.getPlotData = function (projectId) {
-        // FIXME: Replace with an AJAX request
+    // FIXME: Replace with an AJAX request and implement this endpoint
+    this.getPlotData = function (projectId) {
         return ceo_sample_data.plot_data[projectId];
     };
 
-    $scope.loadRandomPlot = function () {
-        var currentProjectPlots = $scope.getPlotData($scope.currentProjectId);
+    this.loadRandomPlot = function () {
+        var currentProjectPlots = this.getPlotData(parseInt(this.currentProjectId));
         var randomIndex = Math.floor(Math.random() * currentProjectPlots.length);
         var newPlot = currentProjectPlots[randomIndex].plot;
         var newSamples = currentProjectPlots[randomIndex].samples;
-        $scope.currentPlot = newPlot;
-        $scope.currentSamples = newSamples;
-        $scope.userSamples = {};
+        this.currentPlot = newPlot;
+        this.currentSamples = newSamples;
+        this.userSamples = {};
         utils.disable_element("new-plot-button");
         utils.enable_element("flag-plot-button");
         utils.disable_element("save-values-button");
         map_utils.draw_buffer(newPlot.center, newPlot.radius);
         map_utils.draw_points(newSamples);
         console.info(map_utils.get_view_extent() + "***********");
-        window.open("geo-dash?title=" + $scope.currentProject.name
-                    + "&pid=" + $scope.currentProjectId
+        window.open("geo-dash?title=" + this.currentProject.name
+                    + "&pid=" + this.currentProjectId
                     + "&aoi=[" + map_utils.get_view_extent()
                     + "]&daterange=",
                     "_geo-dash");
     };
 
-    $scope.setCurrentValue = function (sampleValue) {
+    this.setCurrentValue = function (sampleValue) {
         var selectedFeatures = map_utils.get_selected_samples();
         if (selectedFeatures) {
             var samples = selectedFeatures.getArray();
@@ -95,11 +93,11 @@ dashboard.controller = function ($scope, $http) {
             for (var i = 0; i < samples.length; i++) {
                 var sample = samples[i];
                 var sampleId = sample.get("sample_id");
-                $scope.userSamples[sampleId] = sampleValue.id;
+                this.userSamples[sampleId] = sampleValue.id;
                 map_utils.highlight_sample(sample, sampleValue.color)
             }
             selectedFeatures.clear();
-            if (Object.keys($scope.userSamples).length == $scope.currentSamples.length) {
+            if (Object.keys(this.userSamples).length == this.currentSamples.length) {
                 utils.enable_element("save-values-button");
             }
         } else {
@@ -107,40 +105,36 @@ dashboard.controller = function ($scope, $http) {
         }
     };
 
-    $scope.saveValues = function () {
+    this.saveValues = function () {
         var userId = parseInt(document.getElementById("user-id").value);
-        var plotId = $scope.currentPlot.id;
-        var imagery = $scope.currentProject.imagery;
-        var userSamples = JSON.stringify($scope.userSamples, null, 4);
+        var plotId = this.currentPlot.id;
+        var imagery = this.currentProject.imagery;
+        var userSamples = JSON.stringify(this.userSamples, null, 4);
         $http.post("add-user-samples", [userId, plotId, imagery, userSamples])
             .then(function successCallback(response) {
                 alert("Your assignments have been saved to the database.");
                 utils.enable_element("new-plot-button");
                 utils.disable_element("flag-plot-button");
                 utils.disable_element("save-values-button");
-                $scope.currentPlot = null;
+                this.currentPlot = null;
                 map_utils.disable_selection();
-                $scope.loadRandomPlot();
+                this.loadRandomPlot();
             }, function errorCallback(response) {
                 console.log(response.data);
-                alert("Error saving your assignments to the database. See console for more information.");
+                alert("Error saving your assignments to the database. See console for details.");
             });
     };
 
-    $scope.flagPlot = function () {
-        var plotId = $scope.currentPlot.id;
+    this.flagPlot = function () {
+        var plotId = this.currentPlot.id;
         $http.post("flag-plot", plotId)
             .then(function successCallback(response) {
                 alert("Plot " + plotId + " has been flagged");
-                $scope.loadRandomPlot();
+                this.loadRandomPlot();
             }, function errorCallback(response) {
                 console.log(response.data);
-                alert("Error flagging plot as bad. See console for more information.");
+                alert("Error flagging plot as bad. See console for details.");
             });
     };
 
-};
-
-angular
-    .module("collectEarth")
-    .controller("dashboard.controller", dashboard.controller);
+}]);
--- src/main/resources/template/freemarker/dashboard.ftl
+++ src/main/resources/template/freemarker/dashboard.ftl
@@ -2,24 +2,24 @@
 
 <script type="text/javascript" src="js/dashboard.js"></script>
 
-<div id="dashboard" ng-attr-id="dashboard" ng-controller="dashboard.controller">
+<div id="dashboard" ng-app="dashboard" ng-controller="DashboardController as dashboard" ng-init="dashboard.initialize()">
     <input id="quit-button" class="button" type="button" name="dashboard-quit" value="Quit" onclick="window.location='select-project'">
     <div id="image-analysis-pane"></div>
     <div id="sidebar">
         <div id="sidebar-contents">
             <fieldset>
                 <legend>1. Select Project</legend>
-                <select id="project-id" name="project-id" size="1" ng-model="currentProjectId" ng-change="switchProject()">
-                    <option ng-repeat="project in projectList" value="{{ project.id }}">{{ project.name }}</option>
+                <select id="project-id" name="project-id" size="1" ng-model="dashboard.currentProjectId" ng-change="dashboard.switchProject()">
+                    <option ng-repeat="project in dashboard.projectList" value="{{ project.id }}">{{ project.name }}</option>
                 </select>
-                <input id="new-plot-button" class="button" type="button" name="new-plot" value="2. Analyze New Plot" ng-click="loadRandomPlot()">
+                <input id="new-plot-button" class="button" type="button" name="new-plot" value="2. Analyze New Plot" ng-click="dashboard.loadRandomPlot()">
             </fieldset>
             <fieldset>
                 <legend>3. Assign Values</legend>
                 <ul>
-                    <li ng-repeat="sample in currentProject.sample_values">
-                        <input type="button" id="{{ sample.id }}" name="{{ sample.value + '_' + sample.id }}" value="{{ sample.value }}" style="border-left:1.5rem solid {{ sample.color }}"
-                               ng-click="setCurrentValue(sample)">
+                    <li ng-repeat="sample in dashboard.currentProject.sample_values">
+                        <input type="button" id="{{ sample.id }}" name="{{ sample.value + '_' + sample.id }}" value="{{ sample.value }}"
+                               style="border-left:1.5rem solid {{ sample.color }}" ng-click="dashboard.setCurrentValue(sample)">
                     </li>
                 </ul>
                 <div id="final-plot-options">
@@ -29,14 +29,16 @@
                                 <td>4.</td>
                                 <td>Either</td>
                                 <td>
-                                    <input id="save-values-button" class="button" type="button" name="save-values" value="Save Assignments" ng-click="saveValues()" style="opacity:0.5" disabled>
+                                    <input id="save-values-button" class="button" type="button" name="save-values" value="Save Assignments"
+                                           ng-click="dashboard.saveValues()" style="opacity:0.5" disabled>
                                 </td>
                             </tr>
                             <tr>
                                 <td> </td>
                                 <td>or</td>
                                 <td>
-                                    <input id="flag-plot-button" class="button" type="button" name="flag-plot" value="Flag Plot as Bad" ng-click="flagPlot()" style="opacity:0.5" disabled>
+                                    <input id="flag-plot-button" class="button" type="button" name="flag-plot" value="Flag Plot as Bad"
+                                           ng-click="dashboard.flagPlot()" style="opacity:0.5" disabled>
                                 </td>
                             </tr>
                         </tbody>
@@ -45,7 +47,7 @@
             </fieldset>
         </div>
     </div>
-    <div id="imagery-info"><p>{{ currentProject.attribution }}</p></div>
+    <div id="imagery-info"><p>{{ dashboard.currentProject.attribution }}</p></div>
     <input id="user-id" type="hidden" name="user-id" value=${user_id}>
     <input id="initial-project-id" type="hidden" name="initial-project-id" value=${project_id}>
 </div>
