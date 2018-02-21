8ff2fc192248c387847ae6c95f062cd661a9852a
Billy Ashmall <billy.ashmall@nasa.gov>
2018-02-21 16:01:15 -0600

    Updated Configure button on project page to use new editor

--- src/main/resources/public/js/project.js
+++ src/main/resources/public/js/project.js
@@ -118,7 +118,7 @@ angular.module("project", []).controller("ProjectController", ["$http", function
     };
 
     this.configureGeoDash = function () {
-        window.open(this.root + "/geo-dash?editable=true&"
+        window.open(this.root + "/widget-layout-editor?editable=true&"
                     + encodeURIComponent("title=" + this.details.name
                                          + "&pid=" + this.details.id
                                          + "&aoi=[" + map_utils.get_plot_extent(this.plotList[0].center, this.details.plotSize)
