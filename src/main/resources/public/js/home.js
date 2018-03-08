a2b79f4159ca12c1fbf4f4c64ef388221753d9c8
Gary Johnson <gwjohnso@uvm.edu>
2018-03-08 17:31:41 -0500

    Removed deprecated code from home.js.

--- src/main/resources/public/js/home.js
+++ src/main/resources/public/js/home.js
@@ -1,17 +1,9 @@
-angular.module("home", []).controller("HomeController", ["$scope", "$http", "$window", function HomeController($scope, $http, $window) {
+angular.module("home", []).controller("HomeController", ["$http", function HomeController($http) {
     this.root = "";
     this.userId = "";
     this.institutionList = null;
     this.projectList = null;
-    this.userList = null;
     this.imageryList = null;
-    this.showPanel = true;
-    this.mapWidth = "75%";
-    this.toggleValue = "<<";
-    this.btnHolderWidth = "25%";
-    this.windowWidth;
-    this.mobileDisplay = "block";
-    this.togglebtn = "auto";
 
     this.getInstitutionList = function () {
         $http.get(this.root + "/get-all-institutions")
@@ -23,8 +15,8 @@ angular.module("home", []).controller("HomeController", ["$scope", "$http", "$wi
             });
     };
 
-    this.getProjectList = function (userId) {
-        $http.get(this.root + "/get-all-projects?userId=" + userId + "&institutionId=")
+    this.getProjectList = function () {
+        $http.get(this.root + "/get-all-projects?userId=" + this.userId)
             .then(angular.bind(this, function successCallback(response) {
                 this.projectList = response.data;
                 this.initialize(this.root, this.userId);
@@ -34,18 +26,8 @@ angular.module("home", []).controller("HomeController", ["$scope", "$http", "$wi
             });
     };
 
-    this.getUserList = function () {
-        $http.get(this.root + "/get-all-users")
-            .then(angular.bind(this, function successCallback(response) {
-                this.userList = response.data;
-            }), function errorCallback(response) {
-                console.log(response);
-                alert("Error retrieving the user list. See console for details.");
-            });
-    };
-
     this.getImageryList = function () {
-        $http.get(this.root + "/get-all-imagery?institutionId=")
+        $http.get(this.root + "/get-all-imagery")
             .then(angular.bind(this, function successCallback(response) {
                 this.imageryList = response.data;
                 this.initialize(this.root, this.userId);
@@ -55,105 +37,20 @@ angular.module("home", []).controller("HomeController", ["$scope", "$http", "$wi
             });
     };
 
-    this.updateMapSize = function () {
-        map_utils.map_ref.updateSize();
-    };
-
-    // FIXME: why is $scope here?
-    this.togglePanel = function () {
-        if ($scope.home.windowWidth <= 600) {
-            if (this.showPanel == true) {
-                this.showPanel = false;
-                this.mapWidth = "100%";
-                this.toggleValue = ">>";
-                this.btnHolderWidth = "0%";
-                this.mobileDisplay = "block";
-                this.togglebtn = "auto";
-            } else {
-                this.showPanel = true;
-                this.mapWidth = "0%";
-                this.toggleValue = "<<";
-                this.btnHolderWidth = "100%";
-                this.mobileDisplay = "none";
-                this.togglebtn = "0px";
-            }
-        } else {
-            this.mobileDisplay = "block";
-            this.togglebtn = "auto";
-            if (this.showPanel == true) {
-                this.showPanel = false;
-                this.mapWidth = "100%";
-                this.toggleValue = ">>";
-                this.btnHolderWidth = "0%";
-            } else {
-                this.showPanel = true;
-                this.mapWidth = "75%";
-                this.toggleValue = "<<";
-                this.btnHolderWidth = "25%";
-            }
-        }
-        // FIXME: use this.updateMapSize()
-        $scope.$$postDigest(this.updateMapSize());
-    };
-
-    this.checkResizing = function () {
-        if ($window.innerWidth <= 600) {
-            if(this.showPanel == false) {
-                this.mapWidth = "100%";
-                this.toggleValue = ">>";
-                this.btnHolderWidth = "0%";
-                this.mobileDisplay = "block";
-                this.togglebtn = "auto";
-            } else {
-                this.mapWidth = "0%";
-                this.toggleValue = "<<";
-                this.btnHolderWidth = "100%";
-                this.mobileDisplay = "none";
-                this.togglebtn = "0px";
-            }
-        } else {
-            this.mobileDisplay = "block";
-            this.togglebtn = "auto";
-            if (this.showPanel == false) {
-                this.mapWidth = "100%";
-                this.toggleValue = ">>";
-                this.btnHolderWidth = "0%";
-            } else {
-                this.mapWidth = "75%";
-                this.toggleValue = "<<";
-                this.btnHolderWidth = "25%";
-            }
-        }
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
-        $scope.home.updateMapSize();
-    };
-
     this.initialize = function (documentRoot, userId) {
-        // Make the current documentRoot globally available
+        // Make the current documentRoot and userId globally available
         this.root = documentRoot;
         this.userId = userId;
-        this.windowWidth = $window.innerWidth;
-
-        // FIXME: What is this?
-        angular.element($window).bind("resize", function () {
-            $scope.home.windowWidth = $window.innerWidth;
-            $scope.home.checkResizing();
-        });
 
         if (this.imageryList == null) {
             // Load the imageryList
             this.getImageryList();
         } else if (this.projectList == null) {
-            // Load the projectList for this userId
-            this.getProjectList(userId);
+            // Load the projectList
+            this.getProjectList();
         } else {
             // Load the institutionList
             this.getInstitutionList();
-
-            // Load the userList
-            this.getUserList();
 
             // Display the project map
             map_utils.digital_globe_base_map({div_name: "home-map-pane",
@@ -166,15 +63,6 @@ angular.module("home", []).controller("HomeController", ["$scope", "$http", "$wi
 
             // Draw markers on the map for each project
             map_utils.draw_project_markers(this.projectList, this.root);
-        }
-
-        if ($window.innerWidth <= 600) {
-            this.showPanel = false;
-            try {
-                this.checkResizing();
-            } catch (e) {
-                console.error("Call to checkResizing() failed.");
-            }
         }
     };
 
