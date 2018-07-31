d3fec6d941c1d41dabfd711d154b5a4d6dfff821
NDC\gtondapu <githika.tondapu@nasa.gov>
2018-07-31 16:24:39 -0500

    edited ceo_tables

--- src/main/resources/sql/build_ceo_tables.sql
+++ src/main/resources/sql/build_ceo_tables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE projects (
   id                serial primary key,
-  institution_id       integer not null references institutions (id) on delete cascade on update cascade,
+  institution_id    integer not null references institutions (id) on delete cascade on update cascade,
   availability      text,
   name              text not null,
   description       text,
@@ -17,29 +17,22 @@ CREATE TABLE projects (
   sample_distribution text,
   samples_per_plot  integer,
   sample_resolution float,
-  sample_values     jsonb,
-  archived          boolean default false
+  sample_values     jsonb
 );
 
 CREATE TABLE plots (
   id         serial primary key,
   project_id integer not null references projects (id) on delete cascade on update cascade,
   center     geometry(Point,4326),
-  user_id    integer,
-  flagged    boolean default false,
-  analyses   integer default 0
+  flagged    integer default 0,
+  assigned   integer default 0
 );
-
-CREATE INDEX plots_project_id ON plots (project_id);
-CREATE INDEX plots_analyses ON plots (analyses);
 
 CREATE TABLE samples (
   id      serial primary key,
   plot_id integer not null references plots (id) on delete cascade on update cascade,
   point   geometry(Point,4326)
 );
-
-CREATE INDEX samples_plot_id ON samples (plot_id);
 
 CREATE TABLE imagery (
     id              serial primary key,
@@ -80,23 +73,36 @@ CREATE TABLE roles {
     title   text not null
 };
 
-CREATE INDEX mapcha_users_email ON mapcha.users (email);
+CREATE TABLE user_plots(
+	id          serial primary key,
+	user_id     integer not null references users (id) on delete cascade on update cascade,
+    plot_id     integer not null references plots (id) on delete cascade on update cascade,
+    flagged     boolean default false,
+	confidence  integer default 0 for values from (0) to (100),
+	collection_time timestamp with time zone   
+)
 
-CREATE TABLE mapcha.user_samples (
-  id           serial primary key,
-  user_id      integer not null references mapcha.users (id) on delete cascade on update cascade,
-  sample_id    integer not null references mapcha.samples (id) on delete cascade on update cascade,
-  value_id     integer not null references mapcha.sample_values (id) on delete cascade on update cascade,
-  imagery_id   integer not null references mapcha.imagery (id) on delete cascade on update cascade,
-  date         date
-);
+CREATE TABLE sample_values(
+	id             serial primary key,
+	user_plot_id   integer not null references user_plots (id) on delete cascade on update cascade,
+    sample_id      integer not null references samples (id) on delete cascade on update cascade,     
+	value          jsonb
+)
 
-CREATE INDEX mapcha_user_samples_user_id ON mapcha.user_samples (user_id);
-CREATE INDEX mapcha_user_samples_sample_id ON mapcha.user_samples (sample_id);
+CREATE INDEX projects_id ON projects (id);
+CREATE INDEX plots_id ON plots (id);
+CREATE INDEX samples_id ON samples (id);
+CREATE INDEX imagery_id ON imagery (id);
+CREATE INDEX users_id ON users (id);
+CREATE INDEX institutions_id ON institutions (id);
+CREATE INDEX institution_users_id ON institution_users (id);
+CREATE INDEX roles_id ON roles (id);
+CREATE INDEX user_plots_id ON user_plots (id);
+CREATE INDEX sample_values_id ON sample_values (id);
 
 -- Populate the mapcha.imagery table
 
-INSERT INTO mapcha.imagery (title, date, url, attribution) VALUES
+INSERT INTO imagery (title, date, url, attribution) VALUES
  ('DigitalGlobeRecentImagery', '2015-01-01'::date, 'http://api.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpcTJ3ZTlyZTAwOWNuam00ZWU3aTkxdWIifQ.9OFrmevVe0YB2dJokKhhdA', 'DigitalGlobe Maps API: Recent Imagery | June 2015 | © DigitalGlobe, Inc'),
  ('DigitalGlobeRecentImagery+Streets', '2015-01-01'::date, 'http://api.tiles.mapbox.com/v4/digitalglobe.nal0mpda/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpcTJ3ZTlyZTAwOWNuam00ZWU3aTkxdWIifQ.9OFrmevVe0YB2dJokKhhdA', 'DigitalGlobe Maps API: Recent Imagery+Streets | June 2015 | © DigitalGlobe, Inc'),
  ('BingAerial', '2016-11-04'::date, 'https://www.bingmapsportal.com', 'Bing Maps API: Aerial | © Microsoft Corporation'),
