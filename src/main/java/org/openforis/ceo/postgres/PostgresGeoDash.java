8de34c73635f59e99adeb8198b37094d2cb11f40
Billy Ashmall <billy.ashmall@nasa.gov>
2018-08-03 10:51:06 -0500

    Organized and commented GeoDash.java code

--- src/main/java/org/openforis/ceo/postgres/PostgresGeoDash.java
+++ src/main/java/org/openforis/ceo/postgres/PostgresGeoDash.java
@@ -18,6 +18,7 @@ public class PostgresGeoDash {
     private final String user = "ceo";
     private final String password = "ceo";
 
+    // returns either the dashboard for a project or an empty dashboard if it has not been configured
     public String geodashId(Request req, Response res) {
         String projectId = req.params(":id");
         String projectTitle = req.queryParams("title");
@@ -25,8 +26,7 @@ public class PostgresGeoDash {
         String SQL = "SELECT * FROM get_project_widgets_by_project_id(?)";
 
         try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(SQL)) {
-
+            PreparedStatement pstmt = conn.prepareStatement(SQL)) {
             pstmt.setInt(1, Integer.parseInt(projectId));
             ResultSet rs = pstmt.executeQuery();
             if(rs.next()) {
@@ -64,13 +64,17 @@ public class PostgresGeoDash {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+
         return this.returnBlank(callback);
     }
 
+    // will be removed once confirmed it is abandoned and not needed
     public String updateDashBoardByID(Request req, Response res) {
         /* Code will go here to update dashboard*/
         return "";
     }
+
+    // Creates a dashboard widget for a specific project
     public String createDashBoardWidgetByID(Request req, Response res) {
         String projectId = req.queryParams("pID");
         String dashboardId = req.queryParams("dashID");
@@ -92,6 +96,7 @@ public class PostgresGeoDash {
         return this.returnBlank(callback);
     }
 
+    // Updates a dashboard widget by widget_id
     public String updateDashBoardWidgetByID(Request req, Response res) {
         String widgetId = req.params(":id");
         String widgetJson = req.queryParams("widgetJSON");
@@ -111,6 +116,7 @@ public class PostgresGeoDash {
         return this.returnBlank(callback);
     }
 
+    // Deletes a dashboard widget by widget_id
     public String deleteDashBoardWidgetByID(Request req, Response res) {
         String widgetId = req.params(":id");
         String callback = req.queryParams("callback");
@@ -128,6 +134,7 @@ public class PostgresGeoDash {
         return this.returnBlank(callback);
     }
     /* Helper functions */
+    // Returns the appended callback string or ""
     private String returnBlank(String callback)
     {
         if (callback != null) {
@@ -136,6 +143,8 @@ public class PostgresGeoDash {
             return "";
         }
     }
+
+    //Returns a connection to the database
     private Connection connect() throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
