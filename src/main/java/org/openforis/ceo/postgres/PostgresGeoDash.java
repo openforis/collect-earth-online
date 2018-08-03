2fd4d5162120898ddd71658bde40fa9cf6bc4e73
Billy Ashmall <billy.ashmall@nasa.gov>
2018-08-03 10:37:45 -0500

    Completed geo-dash java connections to db

--- src/main/java/org/openforis/ceo/postgres/PostgresGeoDash.java
+++ src/main/java/org/openforis/ceo/postgres/PostgresGeoDash.java
@@ -17,14 +17,13 @@ public class PostgresGeoDash {
     private final String url = "jdbc:postgresql://localhost";
     private final String user = "ceo";
     private final String password = "ceo";
-    public Connection connect() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
-    }
+
     public String geodashId(Request req, Response res) {
         String projectId = req.params(":id");
         String projectTitle = req.queryParams("title");
         String callback = req.queryParams("callback");
         String SQL = "SELECT * FROM get_project_widgets_by_project_id(?)";
+
         try (Connection conn = this.connect();
              PreparedStatement pstmt = conn.prepareStatement(SQL)) {
 
@@ -65,34 +64,79 @@ public class PostgresGeoDash {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return "";
+        return this.returnBlank(callback);
     }
 
     public String updateDashBoardByID(Request req, Response res) {
         /* Code will go here to update dashboard*/
         return "";
     }
-
     public String createDashBoardWidgetByID(Request req, Response res) {
+        String projectId = req.queryParams("pID");
         String dashboardId = req.queryParams("dashID");
         String widgetJson = req.queryParams("widgetJSON");
         String callback = req.queryParams("callback");
-        return "";
+        String SQL = "SELECT * FROM add_project_widget(?, ?, ?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+            pstmt.setInt(1, Integer.parseInt(projectId));
+            pstmt.setString(2, dashboardId);
+            pstmt.setString(3, widgetJson);
+            ResultSet rs = pstmt.executeQuery();
+            return this.returnBlank(callback);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return this.returnBlank(callback);
     }
 
     public String updateDashBoardWidgetByID(Request req, Response res) {
-        String dashboardId = req.queryParams("dashID");
         String widgetId = req.params(":id");
         String widgetJson = req.queryParams("widgetJSON");
         String callback = req.queryParams("callback");
-        return "";
+        String SQL = "SELECT * FROM update_project_widget_by_widget_id(?, ?)";
+
+        try (Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+            pstmt.setInt(1, Integer.parseInt(widgetId));
+            pstmt.setString(2, widgetJson);
+            ResultSet rs = pstmt.executeQuery();
+            return this.returnBlank(callback);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return this.returnBlank(callback);
     }
 
     public String deleteDashBoardWidgetByID(Request req, Response res) {
-        String dashboardId = req.queryParams("dashID");
         String widgetId = req.params(":id");
         String callback = req.queryParams("callback");
-        return "";
-    }
+        String SQL = "SELECT * FROM delete_project_widget_by_widget_id(?)";
 
+        try (Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+            pstmt.setInt(1, Integer.parseInt(widgetId));
+            ResultSet rs = pstmt.executeQuery();
+            return this.returnBlank(callback);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return this.returnBlank(callback);
+    }
+    /* Helper functions */
+    private String returnBlank(String callback)
+    {
+        if (callback != null) {
+            return callback + "()";
+        } else {
+            return "";
+        }
+    }
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
