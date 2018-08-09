afd0039886eb9779c56a4679cef8e09d732ea36d
Gary Johnson <lambdatronic@gmail.com>
2018-08-09 12:19:59 -0400

    Updated Server.java to dynamically support the three different database backends.

--- src/main/java/org/openforis/ceo/Server.java
+++ src/main/java/org/openforis/ceo/Server.java
@@ -12,27 +12,27 @@ import com.google.gson.JsonObject;
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
 import java.io.File;
-
-import org.openforis.ceo.env.CeoConfig;
-
-import org.openforis.ceo.local.JsonProjects;
-import org.openforis.ceo.local.JsonImagery;
-import org.openforis.ceo.local.JsonUsers;
-import org.openforis.ceo.local.JsonInstitutions;
-import org.openforis.ceo.local.JsonGeoDash;
-
-import org.openforis.ceo.postgres.PostgresProjects;
-import org.openforis.ceo.postgres.PostgresImagery;
-import org.openforis.ceo.postgres.PostgresUsers;
-import org.openforis.ceo.postgres.PostgresInstitutions;
-import org.openforis.ceo.postgres.PostgresGeoDash;
-
-import org.openforis.ceo.collect.CollectProjects;
 import org.openforis.ceo.collect.CollectImagery;
-import org.openforis.ceo.users.OfUsers;
-import org.openforis.ceo.users.OfGroups;
-
+import org.openforis.ceo.collect.CollectProjects;
+import org.openforis.ceo.db_api.GeoDash;
+import org.openforis.ceo.db_api.Imagery;
+import org.openforis.ceo.db_api.Institutions;
+import org.openforis.ceo.db_api.Projects;
+import org.openforis.ceo.db_api.Users;
+import org.openforis.ceo.env.CeoConfig;
+import org.openforis.ceo.local.JsonGeoDash;
+import org.openforis.ceo.local.JsonImagery;
+import org.openforis.ceo.local.JsonInstitutions;
+import org.openforis.ceo.local.JsonProjects;
+import org.openforis.ceo.local.JsonUsers;
+import org.openforis.ceo.postgres.PostgresGeoDash;
+import org.openforis.ceo.postgres.PostgresImagery;
+import org.openforis.ceo.postgres.PostgresInstitutions;
+import org.openforis.ceo.postgres.PostgresProjects;
+import org.openforis.ceo.postgres.PostgresUsers;
 import org.openforis.ceo.users.CeoAuthFilter;
+import org.openforis.ceo.users.OfGroups;
+import org.openforis.ceo.users.OfUsers;
 import spark.servlet.SparkApplication;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -53,8 +53,9 @@ public class Server implements SparkApplication {
         }
     }
 
+    // FIXME: Make all API method references non-static
     // Sets up Spark's routing table and exception handling rules
-    private static void declareRoutes(Projects projects, Users users, Institutions institutions, Imagery imagery, GeoDash geoDash) {
+    private static void declareRoutes(String databaseType, Projects projects, Imagery imagery, Users users, Institutions institutions, GeoDash geoDash) {
         // Create a configured FreeMarker renderer
         FreeMarkerEngine freemarker = new FreeMarkerEngine(getConfiguration());
 
@@ -66,13 +67,18 @@ public class Server implements SparkApplication {
         // Serve static files from src/main/resources/public/
         staticFileLocation("/public");
 
+        // Allow token-based authentication if users are not logged in and we are using the COLLECT database
+        if (databaseType.equals("COLLECT")) {
+            before("/*", new CeoAuthFilter());
+        }
+
         // Routing Table: HTML pages (with no side effects)
         get("/",                                      Views.home(freemarker));
         get("/home",                                  Views.home(freemarker));
         get("/about",                                 Views.about(freemarker));
         get("/support",                               Views.support(freemarker));
         get("/account/:id",                           Views.account(freemarker));
-        get("/institution/:id",                       Views.institution(freemarker, "local"));
+        get("/institution/:id",                       Views.institution(freemarker, databaseType.equals("COLLECT") ? "remote" : "local"));
         get("/collection/:id",                        Views.collection(freemarker));
         get("/geo-dash",                              Views.geodash(freemarker));
         get("/widget-layout-editor",                  Views.editWidgetLayout(freemarker));
@@ -81,6 +87,7 @@ public class Server implements SparkApplication {
         get("/register",                              Views.register(freemarker));
         get("/password",                              Views.password(freemarker));
         get("/password-reset",                        Views.passwordReset(freemarker));
+        get("/card-test",                             Views.cardTest(freemarker));
 
         // Routing Table: HTML pages (with side effects)
         post("/account/:id",                          (req, res) -> Views.account(freemarker).handle(users.updateAccount(req, res), res));
@@ -136,91 +143,6 @@ public class Server implements SparkApplication {
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
     }
 
-    // Sets up Spark's routing table and exception handling rules for use with Collect and Of-Users
-    // private static void declareRemoteStorageRoutes() {
-    //     // Create a configured FreeMarker renderer
-    //     FreeMarkerEngine freemarker = new FreeMarkerEngine(getConfiguration());
-
-    //     // FIXME: Get deploy/clientkeystore signed by a certificate authority.
-    //     // https://docs.oracle.com/cd/E19509-01/820-3503/ggfen/index.html
-    //     // https://spark.apache.org/docs/latest/security.html
-    //     // secure("deploy/clientkeystore", "ceocert", null, null);
-
-    //     // Serve static files from src/main/resources/public/
-    //     staticFileLocation("/public");
-
-    //     // Allow token-based authentication if users are not logged in
-    //     before("/*", new CeoAuthFilter());
-
-    //     // Routing Table: HTML pages
-    //     get("/",                (req, res) -> { return freemarker.render(Views.home(req, res)); });
-    //     get("/home",            (req, res) -> { return freemarker.render(Views.home(req, res)); });
-    //     get("/about",           (req, res) -> { return freemarker.render(Views.about(req, res)); });
-    //     get("/support",         (req, res) -> { return freemarker.render(Views.support(req, res)); });
-    //     get("/account/:id",     (req, res) -> { return freemarker.render(Views.account(req, res)); });
-    //     post("/account/:id",    (req, res) -> { return freemarker.render(Views.account(OfUsers.updateAccount(req, res), res)); });
-    //     get("/institution/:id", (req, res) -> { return freemarker.render(Views.institution(req, res, "remote")); });
-    //     get("/collection/:id",  (req, res) -> { return freemarker.render(Views.collection(req, res)); });
-    //     get("/geo-dash",        (req, res) -> { return freemarker.render(Views.geodash(req, res)); });
-    //     get("/widget-layout-editor", (req, res) -> { return freemarker.render(Views.editWidgetLayout(req, res)); });
-    //     get("/project/:id",     (req, res) -> { return freemarker.render(Views.project(req, res)); });
-    //     get("/login",           (req, res) -> { return freemarker.render(Views.login(req, res)); });
-    //     post("/login",          (req, res) -> { return freemarker.render(Views.login(OfUsers.login(req, res), res)); });
-    //     get("/register",        (req, res) -> { return freemarker.render(Views.register(req, res)); });
-    //     post("/register",       (req, res) -> { return freemarker.render(Views.register(OfUsers.register(req, res), res)); });
-    //     get("/password",        (req, res) -> { return freemarker.render(Views.password(req, res)); });
-    //     post("/password",       (req, res) -> { return freemarker.render(Views.password(OfUsers.getPasswordResetKey(req, res), res)); });
-    //     get("/password-reset",  (req, res) -> { return freemarker.render(Views.passwordReset(req, res)); });
-    //     post("/password-reset", (req, res) -> { return freemarker.render(Views.passwordReset(OfUsers.resetPassword(req, res), res)); });
-    //     get("/card-test",       (req, res) -> { return freemarker.render(Views.cardTest(req, res)); });
-    //     get("/logout",          (req, res) -> { return freemarker.render(Views.home(OfUsers.logout(req, res), res)); });
-
-    //     // Routing Table: Projects API
-    //     get("/get-all-projects",                (req, res) -> { return CollectProjects.getAllProjects(req, res); });
-    //     get("/get-project-by-id/:id",           (req, res) -> { return CollectProjects.getProjectById(req, res); });
-    //     get("/get-project-plots/:id/:max",      (req, res) -> { return CollectProjects.getProjectPlots(req, res); });
-    //     get("/get-project-stats/:id",           (req, res) -> { return CollectProjects.getProjectStats(req, res); });
-    //     get("/get-unanalyzed-plot/:id",         (req, res) -> { return CollectProjects.getUnanalyzedPlot(req, res); });
-    //     get("/get-unanalyzed-plot-by-id/:projid/:id", (req, res) -> { return CollectProjects.getUnanalyzedPlotById(req, res); });
-    //     get("/dump-project-aggregate-data/:id", (req, res) -> { return CollectProjects.dumpProjectAggregateData(req, res); });
-    //     get("/dump-project-raw-data/:id",       (req, res) -> { return CollectProjects.dumpProjectRawData(req, res); });
-    //     post("/create-project",                 (req, res) -> { return CollectProjects.createProject(req, res); });
-    //     post("/publish-project/:id",            (req, res) -> { return CollectProjects.publishProject(req, res); });
-    //     post("/close-project/:id",              (req, res) -> { return CollectProjects.closeProject(req, res); });
-    //     post("/archive-project/:id",            (req, res) -> { return CollectProjects.archiveProject(req, res); });
-    //     post("/add-user-samples",               (req, res) -> { return CollectProjects.addUserSamples(req, res); });
-    //     post("/flag-plot",                      (req, res) -> { return CollectProjects.flagPlot(req, res); });
-
-    //     // Routing Table: Users API
-    //     get("/get-all-users",                   (req, res) -> { return OfUsers.getAllUsers(req, res); });
-    //     post("/update-user-institution-role",   (req, res) -> { return OfUsers.updateInstitutionRole(req, res); });
-    //     post("/request-institution-membership", (req, res) -> { return OfUsers.requestInstitutionMembership(req, res); });
-
-    //     // Routing Table: Institutions API
-    //     get("/get-all-institutions",        (req, res) -> { return OfGroups.getAllInstitutions(req, res); });
-    //     get("/get-institution-details/:id", (req, res) -> { return OfGroups.getInstitutionDetails(req, res); });
-    //     post("/update-institution/:id",     (req, res) -> { return OfGroups.updateInstitution(req, res); });
-    //     post("/archive-institution/:id",    (req, res) -> { return OfGroups.archiveInstitution(req, res); });
-
-    //     // Routing Table: Imagery API
-    //     get("/get-all-imagery",             (req, res) -> { return CollectImagery.getAllImagery(req, res); });
-    //     post("/add-institution-imagery",    (req, res) -> { return CollectImagery.addInstitutionImagery(req, res); });
-    //     post("/delete-institution-imagery", (req, res) -> { return CollectImagery.deleteInstitutionImagery(req, res); });
-
-    //     // Routing Table: GeoDash API
-    //     get("/geo-dash/id/:id",                  (req, res) -> { return GeoDash.geodashId(req, res); });
-    //     get("/geo-dash/update/id/:id",           (req, res) -> { return GeoDash.updateDashBoardByID(req, res); });
-    //     get("/geo-dash/createwidget/widget",     (req, res) -> { return GeoDash.createDashBoardWidgetByID(req, res); });
-    //     get("/geo-dash/updatewidget/widget/:id", (req, res) -> { return GeoDash.updateDashBoardWidgetByID(req, res); });
-    //     get("/geo-dash/deletewidget/widget/:id", (req, res) -> { return GeoDash.deleteDashBoardWidgetByID(req, res); });
-
-    //     // Routing Table: Page Not Found
-    //     get("*", (req, res) -> { return freemarker.render(Views.pageNotFound(req, res)); });
-
-    //     // Handle Exceptions
-    //     exception(Exception.class, (e, req, res) -> e.printStackTrace());
-    // }
-
     // Maven/Gradle entry point for running with embedded Jetty webserver
     public static void main(String[] args) {
         // Load the SMTP settings for sending reset password emails
@@ -231,88 +153,49 @@ public class Server implements SparkApplication {
         CeoConfig.smtpPort      = smtpSettings.get("smtpPort").getAsString();
         CeoConfig.smtpPassword  = smtpSettings.get("smtpPassword").getAsString();
 
-        // Start the Jetty webserver on port 8080
-        port(8080);
+        // Set up the routing table
+        String databaseType = args[0]; // Should be either "JSON" or "POSTGRES"
 
-        // Create objects of the classes that implement the passed-in databaseType
-        String databaseType = args[0];
-
-        // FIXME: Create these interfaces
-        Projects projects;
-        Users users;
-        Institutions institutions;
-        Imagery imagery;
-        GeoDash geoDash;
-
-        switch (databaseType) {
-        case "JSON":
-            projects = new JsonProjects();
-            users = new JsonUsers();
-            institutions = new JsonInstitutions();
-            imagery = new JsonImagery();
-            geoDash = new JsonGeoDash();
-            break;
-        case "POSTGRES":
-            projects = new PostgresProjects();
-            users = new PostgresUsers();
-            institutions = new PostgresInstitutions();
-            imagery = new PostgresImagery();
-            geoDash = new PostgresGeoDash();
-            break;
-        case "COLLECT":
-            projects = new CollectProjects();
-            users = new CollectUsers();
-            institutions = new CollectInstitutions();
-            imagery = new CollectImagery();
-            geoDash = new CollectGeoDash();
-            break;
-        default:
-            throw new RuntimeException();
-            break;
+        if (databaseType.equals("JSON")) {
+            declareRoutes(databaseType,
+                          new JsonProjects(),
+                          new JsonImagery(),
+                          new JsonUsers(),
+                          new JsonInstitutions(),
+                          new JsonGeoDash());
+        } else if (databaseType.equals("POSTGRES")) {
+            declareRoutes(databaseType,
+                          new PostgresProjects(),
+                          new PostgresImagery(),
+                          new PostgresUsers(),
+                          new PostgresInstitutions(),
+                          new PostgresGeoDash());
+        } else {
+            System.out.println("Usage: gradle run <JSON|POSTGRES>");
+            System.exit(1);
         }
 
-        // Set up the routing table
-        declareRoutes(projects, users, institutions, imagery, geoDash);
+        // Start the Jetty webserver on port 8080
+        port(8080);
     }
 
     // Tomcat entry point
     public void init() {
-        // FIXME: Create these interfaces
-        Projects projects;
-        Users users;
-        Institutions institutions;
-        Imagery imagery;
-        GeoDash geoDash;
-
-        switch (databaseType) {
-        case "JSON":
-            projects = new JsonProjects();
-            users = new JsonUsers();
-            institutions = new JsonInstitutions();
-            imagery = new JsonImagery();
-            geoDash = new JsonGeoDash();
-            break;
-        case "POSTGRES":
-            projects = new PostgresProjects();
-            users = new PostgresUsers();
-            institutions = new PostgresInstitutions();
-            imagery = new PostgresImagery();
-            geoDash = new PostgresGeoDash();
-            break;
-        case "COLLECT":
-            projects = new CollectProjects();
-            users = new CollectUsers();
-            institutions = new CollectInstitutions();
-            imagery = new CollectImagery();
-            geoDash = new CollectGeoDash();
-            break;
-        default:
-            throw new RuntimeException();
-            break;
-        }
+        // FIXME: Create and import these interfaces
+        // FIXME: deadgrep for all uses of Projects., Imagery., Users., Groups., and GeoDash. and replace them with Json*.
+        Projects projects = new CollectProjects();
+        Imagery imagery = new CollectImagery();
+        Users users = new OfUsers();
+        Institutions institutions = new OfGroups();
+        GeoDash geoDash = new JsonGeoDash();
 
         // Set up the routing table
-        declareRoutes(projects, users, institutions, imagery, geoDash);
+        declareRoutes("COLLECT",
+                      new CollectProjects(),
+                      new CollectImagery(),
+                      new OfUsers(),
+                      new OfGroups(),
+                      new JsonGeoDash());
     }
 
 }
