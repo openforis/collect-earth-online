7af35b8dc508a6b574af3793d324da7493171e4d
Yang <yzq.yang@gmail.com>
2018-10-25 11:00:17 -0700

    add timesync related routes, only postgres is supported.

--- src/main/java/org/openforis/ceo/db_api/TimeSync.java
+++ src/main/java/org/openforis/ceo/db_api/TimeSync.java
@@ -6,8 +6,12 @@ import spark.Response;
 public interface TimeSync {
 
     String getVersion(Request req, Response res);
-    // String updateDashBoardById(Request req, Response res);
-    // String createDashBoardWidgetById(Request req, Response res);
-    // String updateDashBoardWidgetById(Request req, Response res);
-    // String deleteDashBoardWidgetById(Request req, Response res);
+    String getAssignedProjects(Request req, Response res);
+    String getPlots(Request req, Response res);
+    String getVerticesForPlot(Request req, Response res);
+    String getVerticesForProject(Request req, Response res);
+    String saveVertex(Request req, Response res);
+    String saveComment(Request req, Response res);
+    String getComment(Request req, Response res);
+
 }
