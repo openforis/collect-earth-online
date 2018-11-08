feda83cc7af494940ccf050f64c51c3c6437719d
Gary Johnson <lambdatronic@gmail.com>
2018-11-07 19:34:33 -0500

    '->" in webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
                         ],
                         plugins: [
                             "@babel/plugin-proposal-class-properties",
-                            'lodash'
+                            "lodash"
                         ]
                     }
                 },
