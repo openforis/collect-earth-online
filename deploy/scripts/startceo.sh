d6edb9f12e615cdab4f005b607debed4c2c32325
Billy Ashmall <billy.ashmall@nasa.gov>
2019-02-07 09:50:13 -0600

    added dir variables in the sh scripts to avoid pathing issues

--- deploy/scripts/startceo.sh
+++ deploy/scripts/startceo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd ../../
+CEO_DIR="/home/openforis/github/collect-earth-online"
+cd $CEO_DIR
 sudo pkill -f 'java -Xmx28672m'
 nohup env MAVEN_OPTS="-Xmx28672m" mvn compile exec:java -Dexec.args=JSON >> ceo.log 2>&1 &
--- deploy/scripts/startgeegateway.sh
+++ deploy/scripts/startgeegateway.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd ../../../gee-gateway
+GEE_DIR="/home/openforis/github/gee-gateway"
+cd $GEE_DIR
 sudo pkill -f 'python2 run.py'
 nohup sudo python2 run.py >> gee-gateway.log 2>&1 & 
