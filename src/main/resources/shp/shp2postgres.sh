db39ff0ab76077bf4b9e9d751225488a60bb4c18
Matt Spencer <sirmspencer@gmail.com>
2019-03-06 16:06:04 -0800

    fix sh script

--- src/main/resources/shp/shp2postgres.sh
+++ src/main/resources/shp/shp2postgres.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 unzip -o -d $1 $1.zip
 cd $1
-sname=$(find . -type f -iname "*.shp" -exec basename {} .shp ';')
 
-shp2pgsql -s 4326 $sname $1_shp | sed -e 's/-/_/g' | PGPASSWORD=ceo psql -d ceo --host=localhost -p5432 -U ceo
+sname=$(find . -type f -iname "*.shp" -exec basename {} .shp ';')
+tname=`echo $1_shp | sed -e 's/-/_/g'`
+
+shp2pgsql -d -s 4326 $sname $tname | PGPASSWORD=ceo psql -d ceo --host=localhost -p5432 -U ceo
