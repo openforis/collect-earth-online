3e0a3f1f0a90c97006616ff070635f5e3512eb13
Matt Spencer <sirmspencer@gmail.com>
2019-05-24 14:37:02 -0700

    update arrow size

--- src/main/resources/public/jsx/home.js
+++ src/main/resources/public/jsx/home.js
@@ -178,10 +178,10 @@ class MapPanel extends React.Component {
                     >
                         <div className="empty-div" style={{ height: "50vh" }}/>
                         <div className="my-auto no-gutters text-center">
-                            <div className={this.props.showSidePanel ? "" : "d-none"}>
+                            <div className={this.props.showSidePanel ? "" : "d-none"} style={{ fontSize: ".8rem" }}>
                                 {"\u25C0"}
                             </div>
-                            <div className={this.props.showSidePanel ? "d-none" : ""}>
+                            <div className={this.props.showSidePanel ? "d-none" : ""} style={{ fontSize: ".8rem" }}>
                                 {"\u25B6"}
                             </div>
                         </div>
