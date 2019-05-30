4dc744c43a978d9d70dc5c1389fde5f2578a18cf
Matt Spencer <sirmspencer@gmail.com>
2019-05-30 16:58:48 -0700

    check for no samples JSON

--- src/main/java/org/openforis/ceo/local/JsonProjects.java
+++ src/main/java/org/openforis/ceo/local/JsonProjects.java
@@ -1215,11 +1215,17 @@ public class JsonProjects implements Projects {
                                 return newSample;
                             })
                         .collect(intoJsonArray);
-
                     newPlot.add("samples", newSamples);
                     return newPlot;
                 })
             .collect(intoJsonArray);
+
+        var noSamplesCnt = filterJsonArray(newPlots, p -> p.get("samples").getAsJsonArray().size() == 0).size();
+        if (noSamplesCnt > 0) {
+            throw new  RuntimeException("This files used do not have correctly overlapping data. "
+                                        + noSamplesCnt
+                                        + " plots are missing sample data");
+        }
 
         // Write the plot data to a new plot-data-<id>.json file
         writeJsonFile("plot-data-" + newProject.get("id").getAsString() + ".json", newPlots);
