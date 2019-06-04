667262c3294a8e021b7e7466828d11b981c5c7cb
NDC\gtondapu <githika.tondapu@nasa.gov>
2019-06-04 13:46:44 -0500

    linted few jsx files

--- src/main/resources/public/jsx/project-dashboard.js
+++ src/main/resources/public/jsx/project-dashboard.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { mercator, ceoMapStyles } from "../js/mercator-openlayers.js";
-import { utils } from "../js/utils.js";
 
 import { convertSampleValuesToSurveyQuestions } from "./utils/surveyUtils";
 
@@ -14,17 +13,13 @@ class ProjectDashboard extends React.Component {
             imageryList: [],
             mapConfig: null,
             plotList: [],
-            lonMin: "",
-            latMin: "",
-            lonMax: "",
-            latMax: "",
             dateCreated: null,
             datePublished: null,
             dateClosed: null,
             dateArchived: null,
-            isMapShown: false
+            isMapShown: false,
         };
-    };
+    }
 
     componentDidMount() {
         this.getProjectById(this.props.projectId);
@@ -33,12 +28,16 @@ class ProjectDashboard extends React.Component {
         this.getPlotList(this.props.projectId, 100);//100 is the number of plots you want to see on the map
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
         if (this.state.imageryList.length > 0 && this.state.projectDetails.id && !this.state.isMapShown) {
-            this.setState({ projectDetails: { ...this.state.projectDetails, 
-                                                baseMapSource:  this.state.projectDetails.baseMapSource 
-                                                                || this.state.imageryList[0].title},
-                           isMapShown: true });
+            this.setState({
+                projectDetails: {
+                    ...this.state.projectDetails,
+                    baseMapSource:  this.state.projectDetails.baseMapSource
+                                                                || this.state.imageryList[0].title,
+                },
+                isMapShown: true,
+            });
             this.showProjectMap();
         }
         if (this.state.isMapShown && this.state.plotList.length > 0) {
@@ -50,19 +49,19 @@ class ProjectDashboard extends React.Component {
         fetch(this.props.documentRoot + "/get-project-by-id/" + projectId)
             .then(response => {
                 if (response.ok) {
-                    return response.json()
+                    return response.json();
                 } else {
                     console.log(response);
                     alert("Error retrieving the project info. See console for details.");
                 }
             })
             .then(data => {
-                if (data == "") {
+                if (data === "") {
                     alert("No project found with ID " + projectId + ".");
                     window.location = this.props.documentRoot + "/home";
                 } else {
                     const newSurveyQuestions = convertSampleValuesToSurveyQuestions(data.sampleValues);
-                    this.setState({projectDetails: { ...data, surveyQuestions: newSurveyQuestions }});
+                    this.setState({ projectDetails: { ...data, surveyQuestions: newSurveyQuestions }});
                 }
             });
     }
@@ -71,14 +70,14 @@ class ProjectDashboard extends React.Component {
         fetch(this.props.documentRoot + "/get-project-stats/" + projectId)
             .then(response => {
                 if (response.ok) {
-                    return response.json()
+                    return response.json();
                 } else {
                     console.log(response);
                     alert("Error retrieving project stats. See console for details.");
                 }
             })
             .then(data => {
-                this.setState({stats: data});
+                this.setState({ stats: data });
             });
     }
 
@@ -86,14 +85,14 @@ class ProjectDashboard extends React.Component {
         fetch(this.props.documentRoot + "/get-all-imagery?institutionId=" + institutionId)
             .then(response => {
                 if (response.ok) {
-                    return response.json()
+                    return response.json();
                 } else {
                     console.log(response);
                     alert("Error retrieving the imagery list. See console for details.");
                 }
             })
             .then(data => {
-                this.setState({imageryList: data});
+                this.setState({ imageryList: data });
             });
     }
 
@@ -101,45 +100,50 @@ class ProjectDashboard extends React.Component {
         fetch(this.props.documentRoot + "/get-project-plots/" + projectId + "/" + maxPlots)
             .then(response => {
                 if (response.ok) {
-                    return response.json()
+                    return response.json();
                 } else {
                     console.log(response);
                     alert("Error retrieving plot list. See console for details.");
                 }
             })
             .then(data => {
-                this.setState({plotList: data});
+                this.setState({ plotList: data });
             });
     }
 
     showProjectMap() {
         // Initialize the basemap
-        let mapConfig = mercator.createMap("project-map", [0.0, 0.0], 1, this.state.imageryList);
+        const mapConfig = mercator.createMap("project-map", [0.0, 0.0], 1, this.state.imageryList);
         mercator.setVisibleLayer(mapConfig, this.state.projectDetails.baseMapSource);
         // Display a bounding box with the project's AOI on the map and zoom to it
         mercator.addVectorLayer(mapConfig,
-            "currentAOI",
-            mercator.geometryToVectorSource(mercator.parseGeoJson(this.state.projectDetails.boundary, true)),
-            ceoMapStyles.yellowPolygon);
+                                "currentAOI",
+                                mercator.geometryToVectorSource(mercator.parseGeoJson(this.state.projectDetails.boundary, true)),
+                                ceoMapStyles.yellowPolygon);
         mercator.zoomMapToLayer(mapConfig, "currentAOI");
         // Show the plot centers on the map (but constrain to <= 100 points)
-        this.setState({mapConfig: mapConfig});
+        this.setState({ mapConfig: mapConfig });
     }
 
     render() {
         return (
-            <div id="project-design" className="col-xl-6 col-lg-8 border bg-lightgray mb-5"
-                 style={{display: "contents"}}>
-                <div className="bg-darkgreen mb-3 no-container-margin" style={{width: "100%", margin: "0 10px 0 10px"}}>
+            <div
+                id="project-design"
+                className="col-xl-6 col-lg-8 border bg-lightgray mb-5"
+                style={{ display: "contents" }}
+            >
+                <div className="bg-darkgreen mb-3 no-container-margin" style={{ width: "100%", margin: "0 10px 0 10px" }}>
                     <h1>Project Dashboard</h1>
                 </div>
-                <div style={{display: "inline-flex", width: "100%", margin: "0 10px 0 10px"}}>
-                    <div className="bg-lightgray" style={{margin: "20px", width: "70%"}}>
-                        <ProjectAOI projectId={this.props.projectId} project={this.state}/>
+                <div style={{ display: "inline-flex", width: "100%", margin: "0 10px 0 10px" }}>
+                    <div className="bg-lightgray" style={{ margin: "20px", width: "70%" }}>
+                        <ProjectAOI/>
                     </div>
-                    <div className="bg-lightgray" style={{margin: "20px", width: "30%"}}>
-                        <ProjectStats project={this.state}
-                                      project_stats_visibility={this.props.project_stats_visibility}/>
+                    <div className="bg-lightgray" style={{ margin: "20px", width: "30%" }}>
+                        <ProjectStats
+                            project={this.state}
+                            project_stats_visibility={this.props.project_stats_visibility}
+                        />
                     </div>
 
                 </div>
@@ -155,36 +159,36 @@ function ProjectStats(props) {
                 <h2 className="header px-0">Project Stats</h2>
                 <table className="table table-sm">
                     <tbody>
-                    <tr>
-                        <td>Members</td>
-                        <td>{props.project.stats.members}</td>
-                        <td>Contributors</td>
-                        <td>{props.project.stats.contributors}</td>
-                    </tr>
-                    <tr>
-                        <td>Total Plots</td>
-                        <td>{props.project.projectDetails.numPlots}</td>
-                        <td>Date Created</td>
-                        <td>{props.project.dateCreated}</td>
-                    </tr>
-                    <tr>
-                        <td>Flagged Plots</td>
-                        <td>{props.project.stats.flaggedPlots}</td>
-                        <td>Date Published</td>
-                        <td>{props.project.datePublished}</td>
-                    </tr>
-                    <tr>
-                        <td>Analyzed Plots</td>
-                        <td>{props.project.stats.analyzedPlots}</td>
-                        <td>Date Closed</td>
-                        <td>{props.project.dateClosed}</td>
-                    </tr>
-                    <tr>
-                        <td>Unanalyzed Plots</td>
-                        <td>{props.project.stats.unanalyzedPlots}</td>
-                        <td>Date Archived</td>
-                        <td>{props.project.dateArchived}</td>
-                    </tr>
+                        <tr>
+                            <td>Members</td>
+                            <td>{props.project.stats.members}</td>
+                            <td>Contributors</td>
+                            <td>{props.project.stats.contributors}</td>
+                        </tr>
+                        <tr>
+                            <td>Total Plots</td>
+                            <td>{props.project.projectDetails.numPlots}</td>
+                            <td>Date Created</td>
+                            <td>{props.project.dateCreated}</td>
+                        </tr>
+                        <tr>
+                            <td>Flagged Plots</td>
+                            <td>{props.project.stats.flaggedPlots}</td>
+                            <td>Date Published</td>
+                            <td>{props.project.datePublished}</td>
+                        </tr>
+                        <tr>
+                            <td>Analyzed Plots</td>
+                            <td>{props.project.stats.analyzedPlots}</td>
+                            <td>Date Closed</td>
+                            <td>{props.project.dateClosed}</td>
+                        </tr>
+                        <tr>
+                            <td>Unanalyzed Plots</td>
+                            <td>{props.project.stats.unanalyzedPlots}</td>
+                            <td>Date Archived</td>
+                            <td>{props.project.dateArchived}</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
@@ -192,7 +196,7 @@ function ProjectStats(props) {
     );
 }
 
-function ProjectAOI(props) {
+function ProjectAOI() {
     return (
         <div className="header">
             <div className="col">
@@ -207,9 +211,14 @@ function ProjectAOI(props) {
 
 export function renderProjectDashboardPage(args) {
     ReactDOM.render(
-        <ProjectDashboard documentRoot={args.documentRoot} userId={args.userId} projectId={args.projectId} institutionId={args.institutionId}
-                 project_stats_visibility={args.project_stats_visibility}
-                 project_template_visibility={args.project_template_visibility}/>,
+        <ProjectDashboard
+            documentRoot={args.documentRoot}
+            userId={args.userId}
+            projectId={args.projectId}
+            institutionId={args.institutionId}
+            project_stats_visibility={args.project_stats_visibility}
+            project_template_visibility={args.project_template_visibility}
+        />,
         document.getElementById("project-dashboard")
     );
 }
--- src/main/resources/public/jsx/review-institution.js
+++ src/main/resources/public/jsx/review-institution.js
@@ -295,7 +295,8 @@ class InstitutionDescription extends React.Component {
                                 id="institution-dashboard"
                                 type="button"
                                 className="btn btn-sm btn-outline-lightgreen btn-block mt-0"
-                                onClick={this.gotoInstitutionDashboard}>
+                                onClick={this.gotoInstitutionDashboard}
+                            >
                                 Go to Dashboard
                             </button>
                         </div>
