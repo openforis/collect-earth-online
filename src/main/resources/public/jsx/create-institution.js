097e4b190501dae4eea9b6e88441826c44705f55
NDC\gtondapu <githika.tondapu@nasa.gov>
2019-06-14 15:54:28 -0500

    working on fixing the bug

--- src/main/resources/public/jsx/create-institution.js
+++ src/main/resources/public/jsx/create-institution.js
@@ -26,36 +26,41 @@ class CreateInstitution extends React.Component {
             }
         });
     createInstitution = () => {
-        const insts = this.getInstitutions.filter(inst => inst.name === this.state.newInstitutionDetails.name);
-        if (insts.length > 0) {
-            alert("Duplicate institution! Please review this institution to update it - https://ceodev.servirglobal.net/review-institution/" + insts[0].id);
-        } else {
-            fetch(this.props.documentRoot + "/create-institution",
-                  {
-                      method: "POST",
-                      body: JSON.stringify({
-                          userId: this.props.userId,
-                          name: this.state.newInstitutionDetails.name,
-                          logo: this.state.newInstitutionDetails.logo,
-                          base64Image: this.state.newInstitutionDetails.base64Image,
-                          url: this.state.newInstitutionDetails.url,
-                          description: this.state.newInstitutionDetails.description,
-                      }),
-                  })
-                .then(response => response.ok ? response.json() : Promise.reject(response))
-                .then(data => window.location = this.props.documentRoot + "/review-institution/" + data.id)
-                .catch(response => {
-                    console.log(response);
-                    alert("Error creating institution. See console for details.");
-                });
-        }
-    }
+        fetch(this.props.documentRoot + "/create-institution",
+              {
+                  method: "POST",
+                  body: JSON.stringify({
+                      userId: this.props.userId,
+                      name: this.state.newInstitutionDetails.name,
+                      logo: this.state.newInstitutionDetails.logo,
+                      base64Image: this.state.newInstitutionDetails.base64Image,
+                      url: this.state.newInstitutionDetails.url,
+                      description: this.state.newInstitutionDetails.description,
+                  }),
+              })
+            .then(response => response.ok ? response.json() : Promise.reject(response))
+            .then(data => window.location = this.props.documentRoot + "/review-institution/" + data.id)
+            .catch(response => {
+                console.log(response);
+                alert("Error creating institution. See console for details.");
+            });
+    };
 
-    setInstitutionDetails = (key, newValue) => this.setState({
-        newInstitutionDetails: {
-            ...this.state.newInstitutionDetails, [key]: newValue,
-        },
-    });
+    setInstitutionDetails = (key, newValue) => {
+        if (key === "name") {
+            const insts = this.getInstitutions().filter(inst => inst.name === newValue);
+            if (insts.length > 0) {
+                alert("Duplicate institution! Please review this institution to update it - https://ceodev.servirglobal.net/review-institution/" + insts[0].id);
+            }
+            console.log(insts);
+        } else {
+            this.setState({
+                newInstitutionDetails: {
+                    ...this.state.newInstitutionDetails, [key]: newValue,
+                },
+            });
+        }
+    };
 
     renderButtonGroup = () =>
         <input
