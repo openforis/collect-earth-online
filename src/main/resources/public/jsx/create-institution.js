3ad4c81c27ed385d13764eaf1f8fa80fee3d9946
NDC\gtondapu <githika.tondapu@nasa.gov>
2019-06-17 11:00:21 -0500

    institution duplicate better fix

--- src/main/resources/public/jsx/create-institution.js
+++ src/main/resources/public/jsx/create-institution.js
@@ -30,43 +30,39 @@ class CreateInstitution extends React.Component {
         });
 
     createInstitution = () => {
-        fetch(this.props.documentRoot + "/create-institution",
-              {
-                  method: "POST",
-                  body: JSON.stringify({
-                      userId: this.props.userId,
-                      name: this.state.newInstitutionDetails.name,
-                      logo: this.state.newInstitutionDetails.logo,
-                      base64Image: this.state.newInstitutionDetails.base64Image,
-                      url: this.state.newInstitutionDetails.url,
-                      description: this.state.newInstitutionDetails.description,
-                  }),
-              })
-            .then(response => response.ok ? response.json() : Promise.reject(response))
-            .then(data => window.location = this.props.documentRoot + "/review-institution/" + data.id)
-            .catch(response => {
-                console.log(response);
-                alert("Error creating institution. See console for details.");
-            });
-    };
-
-    getDuplicateInstByName = (k, v) => (k === "name")
-        ? this.state.institutions.find(inst => inst.name === v)
-        : null;
-
-    setInstitutionDetails = (key, newValue) => {
-        const duplicateInst = this.getDuplicateInstByName(key, newValue);
+        const duplicateInst = this.state.institutions.find(inst => inst.name === this.state.newInstitutionDetails.name);
         if (duplicateInst) {
             alert("An institution with this name already exists. "
-                  + "Either select a different name or change the name of the duplicate institution here: "
-                  + window.location.origin + "/review-institution/" + duplicateInst.id);
+                + "Either select a different name or change the name of the duplicate institution here: "
+                + window.location.origin + "/review-institution/" + duplicateInst.id);
         } else {
-            this.setState({
-                newInstitutionDetails: {
-                    ...this.state.newInstitutionDetails, [key]: newValue,
-                },
-            });
+            fetch(this.props.documentRoot + "/create-institution",
+                  {
+                      method: "POST",
+                      body: JSON.stringify({
+                          userId: this.props.userId,
+                          name: this.state.newInstitutionDetails.name,
+                          logo: this.state.newInstitutionDetails.logo,
+                          base64Image: this.state.newInstitutionDetails.base64Image,
+                          url: this.state.newInstitutionDetails.url,
+                          description: this.state.newInstitutionDetails.description,
+                      }),
+                  })
+                .then(response => response.ok ? response.json() : Promise.reject(response))
+                .then(data => window.location = this.props.documentRoot + "/review-institution/" + data.id)
+                .catch(response => {
+                    console.log(response);
+                    alert("Error creating institution. See console for details.");
+                });
         }
+    };
+
+    setInstitutionDetails = (key, newValue) => {
+        this.setState({
+            newInstitutionDetails: {
+                ...this.state.newInstitutionDetails, [key]: newValue,
+            },
+        });
     };
 
     renderButtonGroup = () =>
