5445a75449ef2869c83204d77203abda6a24b4f6
Matt Spencer <sirmspencer@gmail.com>
2019-06-25 11:52:20 -0500

    remove un used imports

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,5 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
-const path = require("path");
-const exec = require("child_process").exec;
 
 module.exports = merge(common, {
     mode: "development",
