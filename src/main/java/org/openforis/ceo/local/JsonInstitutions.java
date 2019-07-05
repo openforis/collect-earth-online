0c5a5b3e5d31ee8c901e458d90a54395e8b068c7
Matt Spencer <sirmspencer@gmail.com>
2019-07-05 13:55:05 -0600

    Port to JSON

--- src/main/java/org/openforis/ceo/local/JsonInstitutions.java
+++ src/main/java/org/openforis/ceo/local/JsonInstitutions.java
@@ -14,6 +14,8 @@ import static org.openforis.ceo.Views.redirectAuth;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Optional;
 import org.openforis.ceo.db_api.Institutions;
@@ -177,6 +179,18 @@ public class JsonInstitutions implements Institutions {
 
     public String archiveInstitution(Request req, Response res) {
         var institutionId = req.params(":id");
+
+        mapJsonFile("project-list.json",
+                    project -> {
+                        if (project.get("institution").getAsString().equals(institutionId)) {
+                            project.addProperty("availability", "archived");
+                            project.addProperty("archived_date", LocalDate.now().toString());
+                            project.addProperty("archived", true);
+                            return project;
+                        } else {
+                            return project;
+                        }
+                    });
 
         mapJsonFile("institution-list.json",
                     institution -> {
