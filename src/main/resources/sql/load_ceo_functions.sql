4a28499195712eccb1c229b204b2797d085adda6
Matt Spencer <sirmspencer@gmail.com>
2020-06-29 11:28:13 -0600

    fix sql logic order

--- src/main/resources/sql/load_ceo_functions.sql
+++ src/main/resources/sql/load_ceo_functions.sql
@@ -628,17 +628,16 @@ CREATE OR REPLACE FUNCTION select_imagery_by_project(_project_rid integer, _user
 
     SELECT imagery_uid, p.institution_rid, visibility, title, attribution, extent, source_config
     FROM imagery i, projects p
-    WHERE i.institution_rid = p.institution_rid
-        AND project_uid = _project_rid
+    WHERE project_uid = _project_rid
         AND archived = FALSE
         AND (visibility = 'public'
-            OR (SELECT count(*) > 0
-                FROM get_all_users_by_institution_id(p.institution_rid)
-                WHERE user_id = _user_rid)
+            OR (i.institution_rid = p.institution_rid
+                AND (SELECT count(*) > 0
+                    FROM get_all_users_by_institution_id(p.institution_rid)
+                    WHERE user_id = _user_rid))
             OR _user_rid = 1)
 
     ORDER BY title
-
 $$ LANGUAGE SQL;
 
 -- Updates imagery attributes
