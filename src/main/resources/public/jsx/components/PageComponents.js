861a64dbc7829f9211b7856199bf851f39d692ad
Matt Spencer <sirmspencer@gmail.com>
2020-07-01 15:21:13 -0600

    About paget ftl -> react

--- src/main/resources/public/jsx/components/PageComponents.js
+++ src/main/resources/public/jsx/components/PageComponents.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function NavigationBar ({ userName, userId, children }) {
+export function NavigationBar({ userName, userId, children }) {
     const uri = window.location.pathname;
     const fullUri = uri + window.location.search;
     const loggedOut = !userName || userName === "" || userName === "guest";
@@ -65,5 +65,75 @@ export function NavigationBar ({ userName, userId, children }) {
             </nav>
             {children}
         </>
+    );
+}
+
+export function LogoBanner() {
+    return (
+        <div id="logo-banner">
+            <div className="row mb-4 justify-content-center">
+                <div className="col-sm-4 text-center">
+                    <img className="img-fluid" id="servir" src="/img/servir-logo.png" />
+                </div>
+            </div>
+            <div className="row justify-content-center mb-2">
+                <div className="col-sm-6 text-center">
+                    <h2>With the support of</h2>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="col-sm-3 text-center my-auto">
+                    <a href="http://openforis.org" target="_blank" rel="noreferrer noopener">
+                        <img className="img-fluid" id="openforis" src="/img/openforis-logo.png" />
+                    </a>
+                </div>
+                <div className="col-sm-3 text-center my-auto">
+                    <a href="http://fao.org" target="_blank" rel="noreferrer noopener">
+                        <img className="img-fluid" id="fao" src="/img/fao.png" />
+                    </a>
+                </div>
+                <div className="col-sm-3 text-center my-auto">
+                    <img className="img-fluid" id="usaid" src="/img/usaid.png" />
+                </div>
+                <div className="col-sm-3 text-center my-auto">
+                    <img className="img-fluid" id="nasa" src="/img/nasa.png" />
+                </div>
+            </div>
+            <div className="row mb-2 justify-content-center">
+                <div className="col-sm-6 text-center">
+                    <h2>In partnership with</h2>
+                </div>
+            </div>
+            <div className="row mb-4 justify-content-center">
+                <div className="col-sm-4 text-center my-auto">
+                    <a href="http://www.silvacarbon.org">
+                        <img className="img-fluid" id="silvacarbon" src="/img/SilvaCarbon.png" />
+                    </a>
+                </div>
+                <div className="col-sm-4 text-center my-auto">
+                    <a href="http://www.sig-gis.com">
+                        <img className="img-fluid" id="sig" src="/img/sig-logo.png" />
+                    </a>
+                </div>
+                <div className="col-sm-4 text-center my-auto">
+                    <a href="https://servir.adpc.net">
+                        <img className="img-fluid" id="servir-mekong" src="/img/servir-mekong-logo.png" />
+                    </a>
+                </div>
+            </div>
+            <div className="row mb-4 justify-content-center">
+                <div className="col-sm-4 text-center my-auto">
+                    <img className="img-fluid" id="google" src="/img/google-logo.png" />
+                </div>
+
+                <div className="col-sm-4 text-center my-auto">
+                    <img className="img-fluid" id="usfs" style={{ width: "60vh" }} src="/img/usfs.png" />
+                </div>
+
+                <div className="col-sm-4 text-center my-auto">
+                    <img className="img-fluid" id="gtac" src="/img/gtac-logo.png" />
+                </div>
+            </div>
+        </div>
     );
 }
