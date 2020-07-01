238fb6ddeaf97eb7999443e5a76c4633e7845dfe
Biplov Bhandari <bionicbiplov45@gmail.com>
2020-07-01 14:43:39 +0545

    separate private and public images for easier navigation

--- src/main/resources/public/jsx/components/ProjectComponents.js
+++ src/main/resources/public/jsx/components/ProjectComponents.js
@@ -110,6 +110,22 @@ export function ProjectAOI({
     projectImageryList,
     setProjectImageryList,
 }) {
+    const createImageryRow = (imageryIds) =>
+        imageryIds.map((imagery, uid) =>
+            <div className="col-md-5 offset-md-1 form-check" key={uid}>
+                <input
+                    className="form-check-input"
+                    id={imagery.id}
+                    onChange={e => setProjectImageryList(e.target.checked
+                        ? [...projectImageryList, imagery.id]
+                        : projectImageryList.filter(img => img !== imagery.id))}
+                    type="checkbox"
+                    disabled={imagery.id === imageryId}
+                    checked={projectImageryList.includes(imagery.id) || imagery.id === imageryId}
+                />
+                <label htmlFor={imagery.id} className="form-check-label">* {imagery.title}</label>
+            </div>
+        );
     return (
         <SectionBlock title="Project AOI">
             <div id="project-aoi">
@@ -196,24 +212,15 @@ export function ProjectAOI({
                                 }
                             </select>
                         </div>
-                        <div className="form-group" id="additional-imagery">
-                            <h3 htmlFor="additional-imagery">Additional Imagery</h3>
-                            <div className="row mt-3">
-                                {imageryList.map((imagery, uid) =>
-                                    <div className="col-md-5 offset-md-1 form-check" key={uid}>
-                                        <input
-                                            className="form-check-input"
-                                            id={imagery.id}
-                                            onChange={e => setProjectImageryList(e.target.checked
-                                                            ? [...projectImageryList, imagery.id]
-                                                            : projectImageryList.filter(img => img !== imagery.id))}
-                                            type="checkbox"
-                                            disabled={imagery.id === imageryId}
-                                            checked={projectImageryList.includes(imagery.id) || imagery.id === imageryId}
-                                        />
-                                        <label htmlFor={imagery.id} className="form-check-label">* {imagery.title}</label>
-                                    </div>
-                                )}
+                        <div className="form-group">
+                            <h3 htmlFor="additional-public-imagery">Public Imagery</h3>
+                            <div className="row mt-3" id="additional-public-imagery">
+                                {createImageryRow(imageryList.filter(imagery => imagery.visibility === "public"))}
+                            </div>
+                            <br />
+                            <h3 htmlFor="additional-private-imagery">Institution Private Imagery</h3>
+                            <div className="row mt-3" id="additional-private-imagery">
+                                {createImageryRow(imageryList.filter(imagery => imagery.visibility === "private"))}
                             </div>
                         </div>
                         <p id="project-imagery-text" className="font-italic ml-2 small">
