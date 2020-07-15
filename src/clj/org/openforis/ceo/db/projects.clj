03c7882e91289a35e89cac1ac2c04431c60a22b6
Gary Johnson <lambdatronic@gmail.com>
2020-07-15 14:44:55 -0400

    Convert db_api/*.java files to db/*.clj files.

---
+++ src/clj/org/openforis/ceo/db/projects.clj
@@ -0,0 +1,25 @@
+(ns org.openforis.ceo.db.projects)
+
+(defn can-collect? [request])
+
+(defn is-proj-admin? [request])
+
+(defn get-all-projects [request])
+
+(defn get-project-by-id [request])
+
+(defn get-project-stats [request])
+
+(defn dump-project-aggregate-data [request])
+
+(defn dump-project-raw-data [request])
+
+(defn publish-project [request])
+
+(defn close-project [request])
+
+(defn archive-project [request])
+
+(defn update-project [request])
+
+(defn create-project [request])
