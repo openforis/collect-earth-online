9a83c1c1d1497b0a2eec12a9973a8b22f18e8e5d
Matt Spencer <sirmspencer@gmail.com>
2020-07-15 13:06:16 -0400

    Move webpack loading into views.cls

--- src/clj/org/openforis/ceo/views.clj
+++ src/clj/org/openforis/ceo/views.clj
@@ -1,6 +1,7 @@
 (ns org.openforis.ceo.views
   (:require [clojure.data.json :as json]
             [clojure.string :as str]
+            [clojure.java.io :as io]
             [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn head [additional-js additional-css]
@@ -11,17 +12,17 @@
    [:meta {:name "description"  :content "Collect Earth Online is an Image Analysis Crowdsourcing Platform by OpenForis and Spatial Informatics Group"}]
    [:meta {:name "keywords"     :content "collect earth online image analysis crowdsourcing platform openforis SIG spatial informatics group"}]
    [:link {:rel "shortcut icon" :href "favicon.ico"}]
-   (apply include-css (conj additional-css
-                            ["/css/cssreset-min.css"
-                             "/css/ie10-viewport-bug-workaround.css" ; TODO remove support for IE 10
-                             "/css/jquery-ui.css" ; TODO remove jquery as a dependency
-                             "/css/datepicker.css" ; TODO remove jquery-ui as a dependency
-                             "/css/bootstrap.min.css"
-                             "/css/custom.css"]))
-   (apply include-js  (conj additional-js
-                            ["/js/ie-emulation-modes-warning.js" ; TODO remove support for IE 10
-                             "/js/html5shiv.js" ; TODO remove support for IE 9
-                             "/js/bootstrap.min.js"]))]) ; TODO remove bootstrap.min.js as a dependency. Only used in header, find a react method.
+   (apply include-css (concat additional-css
+                              ["/css/cssreset-min.css"
+                               "/css/ie10-viewport-bug-workaround.css" ; TODO remove support for IE 10
+                               "/css/jquery-ui.css" ; TODO remove jquery as a dependency
+                               "/css/datepicker.css" ; TODO remove jquery-ui as a dependency
+                               "/css/bootstrap.min.css"
+                               "/css/custom.css"]))
+   (apply include-js  (concat additional-js
+                              ["/js/ie-emulation-modes-warning.js" ; TODO remove support for IE 10
+                               "/js/html5shiv.js" ; TODO remove support for IE 9
+                               "/js/bootstrap.min.js"]))]) ; TODO remove bootstrap.min.js as a dependency. Only used in header, find a react method.
 
 (defn getPage [coll]
   (if (= 1 (count coll)) (first coll) (second coll)))
@@ -37,18 +38,27 @@
        (getPage)
        (kebab->camel)))
 
-(def page->js ; TODO add webpack bundles
-  {"geo-dash"             ["js/highcharts.js" ; TODO use the npm version
-                           "/js/respond.min.js" ; TODO I dont think this is used. Billy imports a grid manager now instead.
-                           "/js/jquery-3.4.1.min.js" ; TODO remove jquery as a dependency
-                           "/js/jquery-ui.min.js"]   ; TODO remove jquery-ui as a dependency
-   "widget-layout-editor" ["/js/jquery-3.4.1.min.js" ; TODO remove jquery as a dependency
-                           "/js/jquery-ui.min.js"]}) ; TODO remove jquery-ui as a dependency
+(defn page->js [page]
+  (let [webpack-files (->> (io/file "target/classes/public/js")
+                           (file-seq)
+                           (map #(str "/js/" (.getName %)))
+                           (remove #(or (str/ends-with? % ".map")
+                                        (str/includes?  % (kebab->camel page))))
+                           (sort-by #(cond
+                                       (str/includes? % "common") -1
+                                       (str/includes? % "~")      0
+                                       :else                      1)))]
+    (concat webpack-files
+            ({"geo-dash"             ["/js/highcharts.js" ; TODO use the npm version
+                                      "/js/respond.min.js" ; TODO remove support for IE 9
+                                      "/js/jquery-3.4.1.min.js" ; TODO remove jquery as a dependency
+                                      "/js/jquery-ui.min.js"]   ; TODO remove jquery-ui as a dependency
+              "widget-layout-editor" ["/js/jquery-3.4.1.min.js"
+                                      "/js/jquery-ui.min.js"]} page))))
 
 (def page->css
   {"geo-dash"             ["/css/geo-dash.css"]
    "widget-layout-editor" ["/css/geo-dash.css"]})
-
 
 (defn cljs-init [page params]
   (let [js-params (json/write-str params)]
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,37 @@
 const path = require("path");
 const fs = require("fs");
 
-// get the output directory according to build tool (maven or gradle)
-// maven -> target
-// gradle -> build
-// Note: make sure both directories don't exist; otherwise the script might not work as expected.
-const outdir = fs.existsSync(path.resolve(__dirname, "target")) ? "target/classes" : "build/resources/main";
+const outdir = "target/classes/public/js";
 
 module.exports = env => ({
     mode: env.dev ? "development" : "production",
     devtool: env.dev ? "inline-source-map" : "source-map",
     watch: env.dev,
     entry: {
-        about                  : path.resolve(__dirname, "src/main/resources/public/jsx/about.js"),
-        account                : path.resolve(__dirname, "src/main/resources/public/jsx/account.js"),
-        collection             : path.resolve(__dirname, "src/main/resources/public/jsx/collection.js"),
-        createInstitution      : path.resolve(__dirname, "src/main/resources/public/jsx/create-institution.js"),
-        createProject          : path.resolve(__dirname, "src/main/resources/public/jsx/create-project.js"),
-        geoDash                : path.resolve(__dirname, "src/main/resources/public/jsx/geo-dash.js"),
-        geoDashHelp            : path.resolve(__dirname, "src/main/resources/public/jsx/geo-dash-help.js"),
-        home                   : path.resolve(__dirname, "src/main/resources/public/jsx/home.js"),
-        institutionDashboard   : path.resolve(__dirname, "src/main/resources/public/jsx/institution-dashboard.js"),
-        login                  : path.resolve(__dirname, "src/main/resources/public/jsx/login.js"),
-        mailingList            : path.resolve(__dirname, "src/main/resources/public/jsx/mailing-list.js"),
-        pageNotFound           : path.resolve(__dirname, "src/main/resources/public/jsx/page-not-found.js"),
-        passwordReset          : path.resolve(__dirname, "src/main/resources/public/jsx/password-reset.js"),
-        passwordRequest        : path.resolve(__dirname, "src/main/resources/public/jsx/password-request.js"),
-        projectDashboard       : path.resolve(__dirname, "src/main/resources/public/jsx/project-dashboard.js"),
-        register               : path.resolve(__dirname, "src/main/resources/public/jsx/register.js"),
-        reviewInstitution      : path.resolve(__dirname, "src/main/resources/public/jsx/review-institution.js"),
-        reviewProject          : path.resolve(__dirname, "src/main/resources/public/jsx/review-project.js"),
-        support                : path.resolve(__dirname, "src/main/resources/public/jsx/support.js"),
-        unsubscribeMailingList : path.resolve(__dirname, "src/main/resources/public/jsx/unsubscribe-mailing-list.js"),
-        widgetLayoutEditor     : path.resolve(__dirname, "src/main/resources/public/jsx/widget-layout-editor.js"),
+        about                  : path.resolve(__dirname, "resources/public/jsx/about.js"),
+        account                : path.resolve(__dirname, "resources/public/jsx/account.js"),
+        collection             : path.resolve(__dirname, "resources/public/jsx/collection.js"),
+        createInstitution      : path.resolve(__dirname, "resources/public/jsx/create-institution.js"),
+        createProject          : path.resolve(__dirname, "resources/public/jsx/create-project.js"),
+        geoDash                : path.resolve(__dirname, "resources/public/jsx/geo-dash.js"),
+        geoDashHelp            : path.resolve(__dirname, "resources/public/jsx/geo-dash-help.js"),
+        home                   : path.resolve(__dirname, "resources/public/jsx/home.js"),
+        institutionDashboard   : path.resolve(__dirname, "resources/public/jsx/institution-dashboard.js"),
+        login                  : path.resolve(__dirname, "resources/public/jsx/login.js"),
+        mailingList            : path.resolve(__dirname, "resources/public/jsx/mailing-list.js"),
+        pageNotFound           : path.resolve(__dirname, "resources/public/jsx/page-not-found.js"),
+        passwordReset          : path.resolve(__dirname, "resources/public/jsx/password-reset.js"),
+        passwordRequest        : path.resolve(__dirname, "resources/public/jsx/password-request.js"),
+        projectDashboard       : path.resolve(__dirname, "resources/public/jsx/project-dashboard.js"),
+        register               : path.resolve(__dirname, "resources/public/jsx/register.js"),
+        reviewInstitution      : path.resolve(__dirname, "resources/public/jsx/review-institution.js"),
+        reviewProject          : path.resolve(__dirname, "resources/public/jsx/review-project.js"),
+        support                : path.resolve(__dirname, "resources/public/jsx/support.js"),
+        unsubscribeMailingList : path.resolve(__dirname, "resources/public/jsx/unsubscribe-mailing-list.js"),
+        widgetLayoutEditor     : path.resolve(__dirname, "resources/public/jsx/widget-layout-editor.js"),
     },
     output: {
-        path: path.resolve(__dirname, outdir + "/public/js"),
+        path: path.resolve(__dirname, outdir),
         filename: "[name].bundle.js",
         library: "[name]",
         libraryTarget: "var",
@@ -85,41 +81,8 @@ module.exports = env => ({
             apply: (compiler) => {
                 compiler.hooks.beforeCompile.tap("BeforeRunPlugin", () => {
                     // Remove old files to prevent conflicts.
-                    fs.readdirSync("./src/main/resources/public/js/")
-                        .filter(f => f.includes(".bundle.js"))
-                        .forEach(f => fs.unlinkSync(path.join("./src/main/resources/public/js/", f)));
-                    fs.readdirSync("./" + outdir + "/public/js/")
-                        .forEach(f => fs.unlinkSync(path.join("./" + outdir + "/public/js/", f)));
-                    fs.readdirSync("./" + outdir + "/template/freemarker/")
-                        .forEach(f => fs.unlinkSync(path.join("./" + outdir + "/template/freemarker/", f)));
-                });
-
-                compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
-                    // Build freemarker files based on compiled js file.
-                    const compiledJsList = fs.readdirSync("./" + outdir + "/public/js")
-                        .filter(a => !a.endsWith(".map"))
-                        .sort(a => a.includes("common") ? -1 : 1) // move common bundle to the top
-                        .sort(a => a.includes("~") ? -1 : 1) // move root bundles to the bottom
-                        .map(b => "<script type=\"text/javascript\" src=\"${root}/js/" + b + "\"></script>");
-                    const ftlFileList = fs.readdirSync("./src/main/resources/template/freemarker");
-
-                    ftlFileList.forEach(f => {
-                        const shortF = f.replace(/.ftl/, "").replace(/-/g, "_");
-                        const bundleList = compiledJsList
-                            .filter(js => js.includes(shortF) || js.includes("common"))
-                            .join("\n");
-
-                        const fileIn = fs.readFileSync(path.join("./src/main/resources/template/freemarker", f), "utf-8");
-                        const jsScripts = "<!-- Auto Inserted Bundles -->\n" + bundleList + "\n<!-- End Auto Inserted Bundles -->";
-                        const fileOut = fileIn.replace(/<!-- Auto Inserted Bundles -->((.|\n|\r\n)*)<!-- End Auto Inserted Bundles -->/, jsScripts);
-
-                        fs.writeFileSync(path.join("./" + outdir + "/template/freemarker", f), fileOut, "utf-8");
-                    });
-
-                    // Copy remaining JS files that do not get compiled.
-                    fs.readdirSync("./src/main/resources/public/js/")
-                        .forEach(f => fs.copyFileSync(path.join("./src/main/resources/public/js/", f),
-                                                      path.join("./" + outdir + "/public/js/", f)));
+                    fs.readdirSync("./" + outdir)
+                        .forEach(f => fs.unlinkSync(path.join("./" + outdir, f)));
                 });
             },
         },
