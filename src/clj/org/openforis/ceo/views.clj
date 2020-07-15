c325163526ef8352fbe31d4e4e4e0c346aace29b
Matt Spencer <sirmspencer@gmail.com>
2020-07-15 13:19:52 -0400

    Remove IE support

--- src/clj/org/openforis/ceo/views.clj
+++ src/clj/org/openforis/ceo/views.clj
@@ -14,48 +14,45 @@
    [:link {:rel "shortcut icon" :href "favicon.ico"}]
    (apply include-css (concat additional-css
                               ["/css/cssreset-min.css"
-                               "/css/ie10-viewport-bug-workaround.css" ; TODO remove support for IE 10
-                               "/css/jquery-ui.css" ; TODO remove jquery as a dependency
-                               "/css/datepicker.css" ; TODO remove jquery-ui as a dependency
+                               "/css/jquery-ui.css"  ; TODO Remove jquery-ui as a dependency.
+                               "/css/datepicker.css" ; TODO Remove jquery-ui as a dependency.
                                "/css/bootstrap.min.css"
                                "/css/custom.css"]))
    (apply include-js  (concat additional-js
-                              ["/js/ie-emulation-modes-warning.js" ; TODO remove support for IE 10
-                               "/js/html5shiv.js" ; TODO remove support for IE 9
-                               "/js/bootstrap.min.js"]))]) ; TODO remove bootstrap.min.js as a dependency. Only used in header, find a react method.
+                              ["/js/bootstrap.min.js"]))]) ; TODO Remove bootstrap.min.js as a dependency. Only used in header, find a react method.
 
+;; TODO This wont be needed if we can flatten the route names for geo-dash (geo-dash/geo-dash -> geo-dash).
 (defn getPage [coll]
   (if (= 1 (count coll)) (first coll) (second coll)))
 
 (defn kebab->camel [kebab]
   (let [pieces (str/split kebab #"-")]
-    (str/join (conj (first pieces)
-                    (map str/capitalize pieces)))))
+    (str/join (concat (first pieces)
+                      (map str/capitalize pieces)))))
 
 (defn uri->page [uri]
   (->> (str/split uri #"/")
        (remove str/blank?)
-       (getPage)
-       (kebab->camel)))
+       (getPage)))
 
 (defn page->js [page]
   (let [webpack-files (->> (io/file "target/classes/public/js")
                            (file-seq)
                            (map #(str "/js/" (.getName %)))
-                           (remove #(or (str/ends-with? % ".map")
-                                        (str/includes?  % (kebab->camel page))))
+                           (filter #(and (str/includes? % (kebab->camel page))
+                                         (not (str/ends-with? % ".map"))))
                            (sort-by #(cond
                                        (str/includes? % "common") -1
                                        (str/includes? % "~")      0
                                        :else                      1)))]
     (concat webpack-files
-            ({"geo-dash"             ["/js/highcharts.js" ; TODO use the npm version
-                                      "/js/respond.min.js" ; TODO remove support for IE 9
-                                      "/js/jquery-3.4.1.min.js" ; TODO remove jquery as a dependency
-                                      "/js/jquery-ui.min.js"]   ; TODO remove jquery-ui as a dependency
+            ({"geo-dash"             ["/js/highcharts.js"       ; TODO Use the npm version.
+                                      "/js/jquery-3.4.1.min.js" ; TODO Remove jquery as a dependency.
+                                      "/js/jquery-ui.min.js"]   ; TODO Remove jquery-ui as a dependency.
               "widget-layout-editor" ["/js/jquery-3.4.1.min.js"
                                       "/js/jquery-ui.min.js"]} page))))
 
+;; TODO Can we just import these from inside each page with import "../css/geo-dash.css"?
 (def page->css
   {"geo-dash"             ["/css/geo-dash.css"]
    "widget-layout-editor" ["/css/geo-dash.css"]})
@@ -74,9 +71,9 @@
                  (head (page->js page) (page->css page))
                  [:body
                   (when-let [flash-message (get-in request [:params :flash_message])]
-                    [:p {:class "alert"} flash-message]) ; TODO this will be moved to the front end for better UX
-                  (when-let [announcement nil] [:p announcement]) ; TODO slurp announcement text from announcement.edn
-                  [:secton {:id "content" :class "container-fluid"} ; TODO, why do all the pages need the same section tag
+                    [:p {:class "alert"} flash-message])            ; TODO This will be moved to the front end for better UX.
+                  (when-let [announcement nil] [:p announcement])   ; TODO Slurp announcement text from announcement.txt.
+                  [:secton {:id "content" :class "container-fluid"} ; TODO This seems out of order with the app div, should the container class be inside each page?
                    [:div#app]]
                   (cljs-init uri (:params request))])})))
 
