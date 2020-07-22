e3c9377b2537847e5988c25ac0fc1c3616b62b91
Matt Spencer <sirmspencer@gmail.com>
2020-07-22 17:46:47 -0400

    Convert partial geodash.java

--- src/clj/org/openforis/ceo/db/geodash.clj
+++ src/clj/org/openforis/ceo/db/geodash.clj
@@ -1,11 +1,48 @@
-(ns org.openforis.ceo.db.geodash)
+(ns org.openforis.ceo.db.geodash
+  (:import java.util.UUID)
+  (:require [clojure.data.json :as json]
+            [org.openforis.ceo.database :refer [call-sql]]
+            [org.openforis.ceo.utils.type-conversion :as tc]
+            [org.openforis.ceo.views :refer [data-response]]))
 
-(defn geodash-id [request])
 
-(defn create-dashboard-widget-by-id [request])
+;;; FIXME, we no longer need dashboardID, projectId should be sufficient
 
-(defn update-dashboard-widget-by-id [request])
+;; FIXME, this function name is not clear
+(defn geodash-id [{:keys [params]}]
+  (let [project-id (tc/str->int (:projectId params))
+        dashboard  (call-sql "get_project_widgets_by_project_id" project-id)]
+    ;; FIXME, we should not need to pass back projectId
+    (data-response {:projectId   project-id
+                    :dashboardID (if (seq dashboard) (:dashboard_id (first dashboard)) (str (UUID/randomUUID)))
+                    :widgets     (mapv :widget dashboard)})))
 
-(defn delete-dashboard-widget-by-id [request])
+(defn create-dashboard-widget-by-id [{:keys [params]}]
+  (let [project-id         (tc/str->int (:projectId params))
+        dashboard-id       (UUID/fromString (:dashboardId params))
+        widget-json-string (json/read-str (:widgetJsonString params))]
+    (call-sql "add_project_widget"
+              project-id
+              dashboard-id
+              widget-json-string)
+    (data-response "")))
+
+(defn update-dashboard-widget-by-id [{:keys [params]}]
+  (let [widget-id          (tc/str->int (:widgetId params))
+        dashboard-id       (UUID/fromString (:dashboardId params))
+        widget-json-string (json/read-str (:widgetJsonString params))]
+    (call-sql "update_project_widget_by_widget_id"
+              widget-id
+              dashboard-id
+              widget-json-string)
+    (data-response "")))
+
+(defn delete-dashboard-widget-by-id [{:keys [params]}]
+  (let [widget-id    (tc/str->int (:widgetId params))
+        dashboard-id (UUID/fromString (:dashboardId params))]
+    (call-sql "delete_project_widget_by_widget_id"
+              widget-id
+              dashboard-id)
+    (data-response "")))
 
 (defn gateway-request [request])
