a6aaf2120eb6f996f139ecdb9949b7ffa6f1bdc4
Matt Spencer <sirmspencer@gmail.com>
2020-07-23 14:42:54 -0400

    wip

--- src/clj/org/openforis/ceo/db/plots.clj
+++ src/clj/org/openforis/ceo/db/plots.clj
@@ -1,19 +1,28 @@
-(ns org.openforis.ceo.db.plots)
+(ns org.openforis.ceo.db.plots
+  (:require [clojure.data.json :as json]
+            [org.openforis.ceo.database :refer [call-sql sql-primitive]]
+            [org.openforis.ceo.utils.type-conversion :as tc]
+            [org.openforis.ceo.views :refer [data-response]]))
 
-(defn get-project-plots [request])
+(defn get-project-plots [{:keys [params]}]
+  (let [project-id (tc/str->int (:projectId params))
+        max-plots  (tc/str->int (:maxPlots params) 1000)]
+    (data-response (map-imagery (call-sql "select_imagery_by_institution" institution-id user-id)
+                                (is-inst-admin-query user-id institution-id))))
+  )
 
-(defn get-project-plot [request])
+(defn get-project-plot [{:keys [params]}])
 
-(defn get-plot-by-id [request])
+(defn get-plot-by-id [{:keys [params]}])
 
-(defn get-next-plot [request])
+(defn get-next-plot [{:keys [params]}])
 
-(defn get-prev-plot [request])
+(defn get-prev-plot [{:keys [params]}])
 
-(defn add-user-samples [request])
+(defn add-user-samples [{:keys [params]}])
 
-(defn flag-plot [request])
+(defn flag-plot [{:keys [params]}])
 
-(defn reset-plot-lock [request])
+(defn reset-plot-lock [{:keys [params]}])
 
-(defn release-plot-locks [request])
+(defn release-plot-locks [{:keys [params]}])
