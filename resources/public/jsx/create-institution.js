9c8d6a1c17f4391770bcd4aab3e5c3ada781902c
Matt Spencer <sirmspencer@gmail.com>
2020-08-12 14:09:52 -0600

    clean up duplicate userId

--- resources/public/jsx/create-institution.js
+++ resources/public/jsx/create-institution.js
@@ -45,7 +45,6 @@ class CreateInstitution extends React.Component {
                           "Content-Type": "application/json",
                       },
                       body: JSON.stringify({
-                          userId: this.props.userId,
                           name: this.state.newInstitutionDetails.name,
                           logo: this.state.newInstitutionDetails.logo,
                           base64Image: this.state.newInstitutionDetails.base64Image,
@@ -104,9 +103,7 @@ class CreateInstitution extends React.Component {
 export function pageInit(args) {
     ReactDOM.render(
         <NavigationBar userName={args.userName} userId={args.userId}>
-            <CreateInstitution
-                userId={args.userId === "" ? -1 : parseInt(args.userId)}
-            />
+            <CreateInstitution/>
         </NavigationBar>,
         document.getElementById("app")
     );
