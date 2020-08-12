c1fa42ca8d370f830008d29b61536c6bb730e6cd
Matt Spencer <sirmspencer@gmail.com>
2020-08-11 19:46:26 -0600

    updates to match other branch

--- src/clj/org/openforis/ceo/utils/part_utils.clj
+++ src/clj/org/openforis/ceo/utils/part_utils.clj
@@ -1,5 +1,12 @@
 (ns org.openforis.ceo.utils.part-utils)
 
+(defn mapm [f coll]
+  (persistent!
+   (reduce (fn [acc cur]
+             (conj! acc (f cur)))
+           (transient {})
+           coll)))
+
 ;; FIXME: stub
 (defn write-file-part-base64 [input-file-name encoded-file output-directory output-file-prefix]
   "")
