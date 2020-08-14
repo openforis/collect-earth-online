6ba6e89533d67df4dc837a1578a301927a6eb828
Matt Spencer <sirmspencer@gmail.com>
2020-08-14 15:18:16 -0600

    Adding helper functions

--- src/clj/org/openforis/ceo/utils/part_utils.clj
+++ src/clj/org/openforis/ceo/utils/part_utils.clj
@@ -3,15 +3,15 @@
   (:require [clojure.java.io :as io]
             [clojure.string  :as str]))
 
-(defn- decode64 [to-decode]
-  (.decode (Base64/getDecoder) (second (str/split to-decode #","))))
-
 (defn mapm [f coll]
   (persistent!
    (reduce (fn [acc cur]
              (conj! acc (f cur)))
            (transient {})
            coll)))
+
+(defn- decode64 [to-decode]
+  (.decode (Base64/getDecoder) (second (str/split to-decode #","))))
 
 (defn write-file-part-base64 [input-file-name encoded-file output-directory output-file-prefix]
   (when input-file-name
@@ -25,3 +25,25 @@
       (with-open [os (io/output-stream full-path)]
         (.write os data 0 (count data)))
       out-name)))
+
+(defn wgs84->mercator
+  "Convert wgs84(4326) lon/lat to mercator(3857) coordinates x/y"
+  [& points]
+  (let [points (mapv (fn [[lon lat]]
+                       [(* lon 111319.490778)
+                        (* (Math/log (Math/tan (* (+ 90.0 lat) (/ Math/PI 360)))) 6378136.98)])
+                     points)]
+    (if (= 1 (count points))
+      (first points)
+      points)))
+
+(defn mercator->wgs84
+  "Convert mercator(3857) coordinates x/y to wgs84(4326) lon/lat"
+  [& points]
+  (let [points (mapv (fn [[x y]]
+                       [(/ x 111319.490778)
+                        (- (/ (Math/atan (Math/exp (/ y 6378136.98))) (/ Math/PI 360)) 90.0)])
+                     points)]
+    (if (= 1 (count points))
+      (first points)
+      points)))
