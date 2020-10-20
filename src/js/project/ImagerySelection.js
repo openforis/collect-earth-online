19dd3a21705d7c885600d01f14f6f03bc9036c82
Matt Spencer <sirmspencer@gmail.com>
2020-10-20 09:28:11 -0700

    review changes from ImagerySelection.js

--- src/js/project/ImagerySelection.js
+++ src/js/project/ImagerySelection.js
@@ -2,108 +2,104 @@ import React from "react";
 
 import {ProjectContext} from "./constants";
 
-export function ImagerySelection() {
-
-    const renderImageryRow = (filteredImageryList, imageryId, projectImageryList, setProjectState) =>
-        filteredImageryList.map((imagery, idx) =>
-            <div className="col-md-5 offset-md-1 form-check" key={idx}>
-                <input
-                    className="form-check-input"
-                    id={imagery.id}
-                    onChange={e => setProjectState({
-                        projectImageryList: e.target.checked
+const renderImageryRow = (filteredImageryList, defaultImageryId, projectImageryList, setProjectState, type) =>
+    <div className="row mt-3 px-5">
+        {filteredImageryList.length > 0
+        ?
+            filteredImageryList.map((imagery, idx) =>
+                <div className="col-md-6 form-check" key={idx}>
+                    <input
+                        className="form-check-input"
+                        id={imagery.id}
+                        onChange={e => setProjectState({
+                            projectImageryList: e.target.checked
                                     ? [...projectImageryList, imagery.id]
                                     : projectImageryList.filter(img => img !== imagery.id),
-                    })}
-                    type="checkbox"
-                    disabled={imagery.id === imageryId}
-                    checked={projectImageryList.includes(imagery.id) || imagery.id === imageryId}
-                />
-                <label htmlFor={imagery.id} className="form-check-label">{imagery.title}</label>
-            </div>
-        );
+                        })}
+                        type="checkbox"
+                        disabled={imagery.id === defaultImageryId}
+                        checked={projectImageryList.includes(imagery.id) || imagery.id === defaultImageryId}
+                    />
+                    <label htmlFor={imagery.id} className="form-check-label">{imagery.title}</label>
+                </div>
+            )
+        : <label>{`No ${type} imagery found.`}</label>}
+    </div>;
 
+export function ImagerySelection() {
     return (
         <ProjectContext.Consumer>
-            {({imageryId, institutionImagery, projectImageryList, setProjectState}) => (
-                <>
-                    {institutionImagery
-                    ?
-                        <div id="project-imagery">
-                            <div className="form-group" id="project-default-imagery">
-                                <h3 htmlFor="project-default-imagery">Default Imagery</h3>
-                                <select
-                                    className="form-control form-control-sm"
-                                    size="1"
-                                    value={imageryId || -1}
-                                    onChange={e => setProjectState({imageryId: parseInt(e.target.value)})}
-                                >
-                                    {institutionImagery.filter(layerConfig => layerConfig.sourceConfig.type !== "PlanetDaily")
-                                        .map((imagery, uid) =>
-                                            <option key={uid} value={imagery.id}>{imagery.title}</option>
-                                        )
-                                    }
-                                </select>
-                            </div>
-                            <div className="form-group">
-                                <h3 htmlFor="additional-public-imagery">Public Imagery</h3>
-                                <div className="row mt-3" id="additional-public-imagery">
-                                    {renderImageryRow(
-                                        institutionImagery.filter(imagery => imagery.visibility === "public"),
-                                        imageryId,
-                                        projectImageryList,
-                                        setProjectState
-                                    )}
-                                </div>
-                                <br />
-                                <h3 htmlFor="additional-private-imagery">Private Institution Imagery*</h3>
-                                <div className="row mt-3" id="additional-private-imagery">
-                                    {renderImageryRow(
-                                        institutionImagery.filter(imagery => imagery.visibility === "private"),
-                                        imageryId,
-                                        projectImageryList,
-                                        setProjectState
-                                    )}
-                                </div>
-                            </div>
-                            <p id="project-imagery-text" className="font-italic ml-2 small">
-                                * Institution imagery will only be visible to institution members, no matter the project visibility.
-                            </p>
+            {({imageryId, institutionImagery, projectImageryList, setProjectState}) =>
+                institutionImagery
+                ?
+                    <div id="project-imagery">
+                        <div className="form-group" id="project-default-imagery">
+                            <h3>Default Imagery</h3>
+                            <select
+                                className="form-control form-control-sm"
+                                size="1"
+                                value={imageryId || -1}
+                                onChange={e => setProjectState({imageryId: parseInt(e.target.value)})}
+                            >
+                                {institutionImagery.filter(layerConfig => layerConfig.sourceConfig.type !== "PlanetDaily")
+                                    .map((imagery, uid) =>
+                                        <option key={uid} value={imagery.id}>{imagery.title}</option>
+                                    )
+                                }
+                            </select>
                         </div>
-                    :
-                        <div id="project-loading-imagery">
-                            <div className="form-group">
-                                <h3 htmlFor="project-loading-imagery">Default Imagery</h3>
-                                <p>Loading Imagery...</p>
-                            </div>
+                        <div className="form-group">
+                            <h3>Public Imagery</h3>
+                            {renderImageryRow(
+                                institutionImagery.filter(imagery => imagery.visibility === "public"),
+                                imageryId,
+                                projectImageryList,
+                                setProjectState,
+                                "public"
+                            )}
+                            <h3>Private Institution Imagery*</h3>
+                            {renderImageryRow(
+                                institutionImagery.filter(imagery => imagery.visibility === "private"),
+                                imageryId,
+                                projectImageryList,
+                                setProjectState,
+                                "institution"
+                            )}
                         </div>
-                    }
-                </>
-            )}
+                        <p id="project-imagery-text" className="font-italic ml-2 small">
+                            * Institution imagery will only be visible to institution members, no matter the project visibility.
+                        </p>
+                    </div>
+                :
+                    <div id="project-loading-imagery">
+                        <div className="form-group">
+                            <h3>Default Imagery</h3>
+                            <p>Loading Imagery...</p>
+                        </div>
+                    </div>
+            }
         </ProjectContext.Consumer>
     );
 }
 
-export function ImageryReview() {
-    const findImageryName = (id, institutionImagery) => {
-        const imagery = institutionImagery.find(ii => ii.id === id);
-        return imagery ? imagery.title : "Imagery Error";
-    };
+const findImageryName = (id, institutionImagery) => {
+    const imagery = institutionImagery.find(ii => ii.id === id);
+    return imagery ? imagery.title : "Imagery Error";
+};
 
+export function ImageryReview() {
     return (
         <ProjectContext.Consumer>
             {({imageryId, projectImageryList, institutionImagery}) =>
-                <div>
-                    <div className="d-flex flex-column">
-                        <label><b>Default Imagery:</b> {findImageryName(imageryId, institutionImagery)}</label>
-                        <label><b>Additional Imagery:</b></label>
-                        <ul>
-                            {projectImageryList
-                                .filter(id => id !== imageryId)
-                                .map(id => <li key={id}>{findImageryName(id, institutionImagery)}</li>)
-                            }
-                        </ul>
-                    </div>
+                <div className="d-flex flex-column">
+                    <label><b>Default Imagery:</b> {findImageryName(imageryId, institutionImagery)}</label>
+                    <label><b>Additional Imagery:</b></label>
+                    <ul>
+                        {projectImageryList
+                            .filter(id => id !== imageryId)
+                            .map(id => <li key={id}>{findImageryName(id, institutionImagery)}</li>)
+                        }
+                    </ul>
                 </div>
             }
         </ProjectContext.Consumer>
