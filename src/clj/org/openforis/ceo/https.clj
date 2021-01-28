ea946a22a7c55d1d9d21ac836d62f586f6ccdcff
Matt Spencer <sirmspencer@gmail.com>
2021-01-28 15:32:11 -0800

    Add missing require

--- src/clj/org/openforis/ceo/https.clj
+++ src/clj/org/openforis/ceo/https.clj
@@ -1,7 +1,8 @@
 (ns org.openforis.ceo.https
-  (:require [clojure.java.io :as io]
+  (:require [clojure.java.io    :as io]
             [clojure.java.shell :as sh]
-            [clojure.string :as str]
+            [clojure.string     :as str]
+            [clojure.tools.cli         :refer [parse-opts]]
             [org.openforis.ceo.logging :refer [log-str]]))
 
 (def path-env (System/getenv "PATH"))
