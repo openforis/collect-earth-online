2aa0ef18e99747ce9b2be43984e3a49e3167e228
Matt Spencer <sirmspencer@gmail.com>
2021-02-19 11:12:49 -0800

    use byte array encoding to create base64 string

--- src/clj/org/openforis/ceo/convert_logos.clj
+++ src/clj/org/openforis/ceo/convert_logos.clj
@@ -1,20 +1,26 @@
 (ns org.openforis.ceo.convert-logos
+  (:import java.util.Base64)
   (:require [clojure.java.io    :as io]
-            [clojure.java.shell :as sh]))
+            [org.openforis.ceo.database :refer [call-sql]]
+            [org.openforis.ceo.utils.type-conversion :as tc]))
+
+(defn encode [file]
+  (with-open [is (io/input-stream file)
+              os (java.io.ByteArrayOutputStream.)]
+    (io/copy is os)
+    (.encodeToString (Base64/getEncoder) (.toByteArray os))))
 
 ;; TODO: Delete this file after migration
 ;; TODO: Delete /resources/public/img/institution-logos after migration
 ;; Call with clojure -m org.openforis.ceo.convert-logos
 (defn -main []
-  (doseq [logo (->> (io/resource "public/img/institution-logos")
+  (doseq [logo (->> (io/resource "/resources/public/img/institution-logos")
                     (io/file)
                     (file-seq)
                     (filter #(.isFile %)))]
     (when-let [id (re-find #"(?<=institution-)\d*" (.getName logo))]
       (println id)
-      (sh/sh "sh"
-             "-c"
-             (format "psql -h localhost -U postgres -d ceo -c \"select add_institution_logo_by_file(%s, '%s'::text)\""
-                     id
-                     (.getPath logo)))))
+      (call-sql "update_institution_logo"
+                (tc/val->int id)
+                (encode logo))))
   (shutdown-agents))
--- src/sql/changes/update_2021-01-15_sql_clean.sql
+++ src/sql/changes/update_2021-01-15_sql_clean.sql
@@ -163,16 +163,6 @@ DROP TYPE IF EXISTS institution_return CASCADE;
 
 ALTER TABLE institutions DROP COLUMN IF EXISTS logo;
 
-CREATE OR REPLACE FUNCTION add_institution_logo_by_file(_institution_id integer, _file_name text)
- RETURNS integer AS $$
-
-    UPDATE institutions
-    SET logo_data = pg_read_binary_file(_file_name)
-    WHERE institution_uid = _institution_id
-    RETURNING institution_uid
-
-$$ LANGUAGE SQL;
-
 -- Clean up
 DELETE FROM institution_users WHERE user_rid = -1;
 
