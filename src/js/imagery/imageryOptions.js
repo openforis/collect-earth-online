d9bd8e6146a216d97cef56b61ec43ae333a2546c
Matt Spencer <sirmspencer@gmail.com>
2021-07-08 11:32:45 -0700

    add june NICFI

--- src/js/imagery/imageryOptions.js
+++ src/js/imagery/imageryOptions.js
@@ -18,7 +18,8 @@ export const nicfiLayers = ["2015-12_2016-05",
                             "2021-02",
                             "2021-03",
                             "2021-04",
-                            "2021-05"];
+                            "2021-05",
+                            "2021-06"];
 
 const outOfRange = (num, low, high) => isNaN(num) || parseInt(num) < low || parseInt(num) > high;
 
