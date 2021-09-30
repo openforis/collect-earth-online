53a0c438cbdcfb53f468d8e36c98b0e51c96972f
RJ Sheperd <rjsheperd@gmail.com>
2021-09-30 11:41:10 -0700

    Update User/QC percent error messages

--- src/js/project/CreateProjectWizard.js
+++ src/js/project/CreateProjectWizard.js
@@ -307,9 +307,9 @@ export default class CreateProjectWizard extends React.Component {
             (userMethod === "percent" && percents.reduce((acc, p) => acc + p, 0) !== 100)
                 && "The assigned plot percentages must equal 100%.",
             (userMethod === "percent" && percents.reduce((acc, p) => (acc || p === 0), false))
-                && "All plot percentages must be greater than 0%.",
+                && "All plot assignment percentages must be greater than 0%.",
             (["overlap", "sme"].includes(qaqcMethod) && percent === 0)
-                && "The assigned Quality Control percentage must be greater than 0.",
+                && "The assigned Quality Control percentage must be greater than 0%.",
             (qaqcMethod === "sme" && smes.length === 0)
                 && "At least one user must be added as an SME.",
             (qaqcMethod === "overlap" && users.length === 1)
