4fc0c359c9f8c90bb32b4aca6c22ff5622074e93
RJ Sheperd <rjsheperd@gmail.com>
2021-10-08 15:35:34 -0700

    Only allow admins to access disagreement page

--- src/clj/collect_earth_online/routing.clj
+++ src/clj/collect_earth_online/routing.clj
@@ -50,7 +50,9 @@
                                               :auth-type   :admin
                                               :auth-action :redirect}
    [:get  "/support"]                        {:handler     (render-page "/support")}
-   [:get  "/user-disagreement"]              {:handler     (render-page "/user-disagreement")}
+   [:get  "/user-disagreement"]              {:handler     (render-page "/user-disagreement")
+                                              :auth-type   :admin
+                                              :auth-action :redirect}
    [:get  "/terms-of-service"]               {:handler     (render-page "/terms-of-service")}
    [:get  "/widget-layout-editor"]           {:handler     (render-page "/widget-layout-editor")
                                               :auth-type   :admin
