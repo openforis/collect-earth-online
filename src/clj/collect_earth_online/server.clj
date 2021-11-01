747ceac74018e63051006c0d39324bf09bdf422c
RJ Sheperd <rjsheperd@gmail.com>
2021-11-01 16:58:09 -0700

    Update server.clj to allow args to be passed from config.edn

--- src/clj/collect_earth_online/server.clj
+++ src/clj/collect_earth_online/server.clj
@@ -1,8 +1,9 @@
 (ns collect-earth-online.server
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.tools.cli  :refer [parse-opts]]
             [ring.adapter.jetty :refer [run-jetty]]
+            [triangulum.cli     :refer [get-cli-options]]
+            [triangulum.config  :refer [get-config]]
             [triangulum.logging :refer [log-str set-log-path!]]
             [collect-earth-online.handler :refer [create-handler-stack]]))
 
@@ -34,49 +35,44 @@
       (try (delete-tmp)
            (catch Exception _)))))
 
-(def cli-options
-  [["-p" "--http-port PORT" "Port for http, default 8080"
-    :default 8080
-    :parse-fn #(if (int? %) % (Integer/parseInt %))
-    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ["-P" "--https-port PORT" "Port for https (e.g. 8443)"
-    :parse-fn #(if (int? %) % (Integer/parseInt %))
-    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ["-m" "--mode MODE" "Production (prod) or development (dev) mode, default prod"
-    :default "prod"
-    :validate [#{"prod" "dev"} "Must be \"prod\" or \"dev\""]]
-   ["-o" "--output-dir DIR" "Output directory for log files. When a directory is not provided, output will be to stdout."
-    :default ""]])
+(def ^:private cli-options
+  {:port       ["-p" "--http-port PORT"   "Port for http, default 8080" :parse-fn #(if (int? %) % (Integer/parseInt %))]
+   :https-port ["-P" "--https-port PORT"  "Port for https (e.g. 8443)"  :parse-fn #(if (int? %) % (Integer/parseInt %))]
 
-(defn start-server! [& args]
-  (let [{:keys [options summary errors]} (parse-opts args cli-options)]
-    (if (seq errors)
+   :mode       ["-m" "--mode MODE"        "Production (prod) or development (dev) mode, default prod"
+                :default "prod"
+                :validate [#{"prod" "dev"} "Must be \"prod\" or \"dev\""]]
+
+   :output-dir ["-o" "--output-dir DIR" "Output directory for log files. When a directory is not provided, output will be to stdout."
+                :default ""]})
+
+(def ^:private cli-actions
+  {:start {:description "Starts the server."
+           :requires    [:port]}
+   :stop  {:description "Stops the server."}})
+
+(defn start-server! [{:keys [port https-port mode output-dir]}]
+  (let [has-key?   (.exists (io/file "./.key/keystore.pkcs12"))
+        ssl?       (and has-key? https-port)
+        handler    (create-handler-stack ssl? (= mode "dev"))
+        config     (merge
+                     {:port  port
+                      :join? false}
+                     (when ssl?
+                       {:ssl?                   true
+                        :ssl-port               https-port
+                        :keystore               "./.key/keystore.pkcs12"
+                        :keystore-type          "pkcs12"
+                        :keystore-scan-interval keystore-scan-interval
+                        :key-password           "foobar"}))]
+    (if (and (not has-key?) https-port)
+      (println "ERROR:\n"
+               "  An SSL key is required if an HTTPS port is specified.\n"
+               "  Create an SSL key for HTTPS or run without the --https-port (-P) option.")
       (do
-        (run! println errors)
-        (println (str "Usage:\n" summary)))
-      (let [mode       (:mode options)
-            has-key?   (.exists (io/file "./.key/keystore.pkcs12"))
-            https-port (:https-port options)
-            ssl?       (and has-key? https-port)
-            handler    (create-handler-stack ssl? (= mode "dev"))
-            config     (merge
-                        {:port  (:http-port options)
-                         :join? false}
-                        (when ssl?
-                          {:ssl?                   true
-                           :ssl-port               https-port
-                           :keystore               "./.key/keystore.pkcs12"
-                           :keystore-type          "pkcs12"
-                           :keystore-scan-interval keystore-scan-interval
-                           :key-password           "foobar"}))]
-        (if (and (not has-key?) https-port)
-          (println "ERROR:\n"
-                   "  An SSL key is required if an HTTPS port is specified.\n"
-                   "  Create an SSL key for HTTPS or run without the --https-port (-P) option.")
-          (do
-            (reset! server (run-jetty handler config))
-            (reset! clean-up-service (start-clean-up-service!))
-            (set-log-path! (:output-dir options))))))))
+        (reset! server (run-jetty handler config))
+        (reset! clean-up-service (start-clean-up-service!))
+        (set-log-path! output-dir)))))
 
 (defn stop-server! []
   (set-log-path! "")
@@ -87,4 +83,8 @@
     (.stop @server)
     (reset! server nil)))
 
-(def -main start-server!)
+(defn -main [& args]
+  (let [{:keys [action options]} (get-cli-options args cli-options cli-actions "server" (get-config :server))]
+    (case action
+      :start (start-server! options)
+      :stop  (stop-server!))))
