972dd2fb8475faf7fc5adf966ea850504f80cf56
RJ Sheperd <rjsheperd@gmail.com>
2021-11-02 10:45:45 -0700

    Adds nrepl port to server startup

--- src/clj/collect_earth_online/server.clj
+++ src/clj/collect_earth_online/server.clj
@@ -1,14 +1,18 @@
 (ns collect-earth-online.server
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
+  (:require [clojure.java.io            :as io]
+            [clojure.edn                :as edn]
+            [clojure.string             :as str]
+            [clojure.tools.nrepl        :as repl]
+            [clojure.tools.nrepl.server :as nrepl]
             [ring.adapter.jetty :refer [run-jetty]]
             [triangulum.cli     :refer [get-cli-options]]
             [triangulum.config  :refer [get-config]]
             [triangulum.logging :refer [log-str set-log-path!]]
             [collect-earth-online.handler :refer [create-handler-stack]]))
 
-(defonce server           (atom nil))
-(defonce clean-up-service (atom nil))
+(defonce ^:private server           (atom nil))
+(defonce ^:private nrepl-server     (atom nil))
+(defonce ^:private clean-up-service (atom nil))
 
 (def ^:private expires-in "1 hour in msecs" (* 1000 60 60))
 (def ^:private keystore-scan-interval 60) ; seconds
@@ -52,6 +56,9 @@
    :stop  {:description "Stops the server."}})
 
 (defn start-server! [{:keys [port https-port mode output-dir]}]
+  (let [port 5555]
+    (reset! nrepl-server (nrepl/start-server :port port))
+    (spit ".nreplport" port))
   (let [has-key?   (.exists (io/file "./.key/keystore.pkcs12"))
         ssl?       (and has-key? https-port)
         handler    (create-handler-stack ssl? (= mode "dev"))
@@ -74,17 +81,29 @@
         (reset! clean-up-service (start-clean-up-service!))
         (set-log-path! output-dir)))))
 
+(defn send-to-nrepl-server! [code]
+  (when-let [port (edn/read-string (slurp ".nreplport"))]
+    (with-open [conn (repl/connect :port port)]
+      (-> (repl/client conn 1000)
+          (repl/message {:op :eval :code code })
+          repl/response-values
+          println))))
+
 (defn stop-server! []
   (set-log-path! "")
   (when @clean-up-service
     (future-cancel @clean-up-service)
     (reset! clean-up-service nil))
+  (when (nil? @server)
+    (send-to-nrepl-server! "(require '[collect-earth-online.server :as server]) (server/stop-server!)"))
   (when @server
     (.stop @server)
-    (reset! server nil)))
+    (reset! server nil)
+    (System/exit 0)))
 
 (defn -main [& args]
   (let [{:keys [action options]} (get-cli-options args cli-options cli-actions "server" (get-config :server))]
     (case action
       :start (start-server! options)
-      :stop  (stop-server!))))
+      :stop  (stop-server!)
+      nil    nil)))
