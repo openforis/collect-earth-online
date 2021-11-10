09edfd8355ac84764ba110d2f72c767b9cd2324e
Matt Spencer <sirmspencer@gmail.com>
2021-11-10 14:06:00 -0800

    Updates to match tri-80

--- src/clj/collect_earth_online/server.clj
+++ src/clj/collect_earth_online/server.clj
@@ -39,49 +39,49 @@
            (catch Exception _)))))
 
 (def ^:private cli-options
-  {:port       ["-p" "--http-port PORT"   "Port for http, default 8080" :parse-fn #(if (int? %) % (Integer/parseInt %))]
-   :https-port ["-P" "--https-port PORT"  "Port for https (e.g. 8443)"  :parse-fn #(if (int? %) % (Integer/parseInt %))]
-   :repl       ["-r" "--repl"             "Starts a REPL server on port 5555"
+  {:port       ["-p" "--http-port PORT"  "Port for http, default 8080"
+                :parse-fn #(if (int? %) % (Integer/parseInt %))]
+   :https-port ["-P" "--https-port PORT" "Port for https (e.g. 8443)"
+                :parse-fn #(if (int? %) % (Integer/parseInt %))]
+   :repl       ["-r" "--repl" "Starts a REPL server on port 5555"
                 :default false]
-
-   :mode       ["-m" "--mode MODE"        "Production (prod) or development (dev) mode, default prod"
+   :mode       ["-m" "--mode MODE" "Production (prod) or development (dev) mode, default prod"
                 :default "prod"
                 :validate [#{"prod" "dev"} "Must be \"prod\" or \"dev\""]]
-
-   :output-dir ["-o" "--output-dir DIR" "Output directory for log files. When a directory is not provided, output will be to stdout."
+   :log-dir    ["-l" "--log-dir DIR" "Directory for log files. When a directory is not provided, output will be to stdout."
                 :default ""]})
 
 (def ^:private cli-actions
   {:start  {:description "Starts the server."
-            :requires    [:port]}
+            :requires    [:http-port]}
    :stop   {:description "Stops the server."}
    :reload {:description "Reloads a running server."}})
 
-(defn start-server! [{:keys [port https-port mode output-dir repl]}]
-  (when repl
-    (println "Starting REPL server on port 5555")
-    (reset! repl-server (start-server {:name :ceo-repl :port 5555 :accept 'clojure.core.server/repl})))
+(defn start-server! [{:keys [http-port https-port mode log-dir repl]}]
   (let [has-key?   (.exists (io/file "./.key/keystore.pkcs12"))
         ssl?       (and has-key? https-port)
         handler    (create-handler-stack ssl? (= mode "dev"))
         config     (merge
-                     {:port  port
-                      :join? false}
-                     (when ssl?
-                       {:ssl?                   true
-                        :ssl-port               https-port
-                        :keystore               "./.key/keystore.pkcs12"
-                        :keystore-type          "pkcs12"
-                        :keystore-scan-interval keystore-scan-interval
-                        :key-password           "foobar"}))]
+                    {:port  http-port
+                     :join? false}
+                    (when ssl?
+                      {:ssl?                   true
+                       :ssl-port               https-port
+                       :keystore               "./.key/keystore.pkcs12"
+                       :keystore-type          "pkcs12"
+                       :keystore-scan-interval keystore-scan-interval
+                       :key-password           "foobar"}))]
     (if (and (not has-key?) https-port)
       (println "ERROR:\n"
                "  An SSL key is required if an HTTPS port is specified.\n"
                "  Create an SSL key for HTTPS or run without the --https-port (-P) option.")
       (do
+        (when repl
+          (println "Starting REPL server on port 5555")
+          (reset! repl-server (start-server {:name :ceo-repl :port 5555 :accept 'clojure.core.server/repl})))
         (reset! server (run-jetty handler config))
         (reset! clean-up-service (start-clean-up-service!))
-        (set-log-path! output-dir)))))
+        (set-log-path! log-dir)))))
 
 (defn stop-server! []
   (set-log-path! "")
@@ -107,7 +107,11 @@
   (send-to-repl-server! "(require 'collect-earth-online.server :reload-all)"))
 
 (defn -main [& args]
-  (let [{:keys [action options]} (get-cli-options args cli-options cli-actions "server" (get-config :server))]
+  (let [{:keys [action options]} (get-cli-options args
+                                                  cli-options
+                                                  cli-actions
+                                                  "server"
+                                                  (get-config :server))]
     (case action
       :start  (start-server! options)
       :stop   (stop-running-server!)
