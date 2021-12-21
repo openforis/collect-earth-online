12253b5f2a39e1d690e2ab016d7869d64b1488c0
Matt Spencer <sirmspencer@gmail.com>
2021-12-21 13:35:09 -0800

    Create a series of sequence function to allow map and filter over objects

--- src/js/utils/sequence.js
+++ src/js/utils/sequence.js
@@ -31,3 +31,107 @@ export function removeFromSet(set, value) {
     set.delete(value);
     return set;
 }
+
+/// Object Functions ///
+
+/**
+ * Returns object where fn is applied to the key value pair. fn must take and return [key, val].
+ * Note, number keys will be sent to fn as a string.
+ * @param {object} obj
+ * @param {function([key, val]): [key, val]} fn
+ * @returns {object}
+ **/
+export function mapObject(obj, fn) {
+    return Object.keys(obj).reduce((acc, cur) => {
+        const [key, val] = fn([cur, obj[cur]]);
+        return {...acc, [key]: val};
+    }, {});
+}
+
+/**
+ * Returns object where fn is applied to just the keys.
+ * Note, number keys will be sent to fn as a string.
+ * @param {object} obj
+ * @param {function(key): key} fn
+ * @returns {object}
+ **/
+export function mapKeys(obj, fn) {
+    return Object.keys(obj).reduce((acc, cur) => {
+        const key = fn(cur);
+        return {...acc, [key]: obj[cur]};
+    }, {});
+}
+
+/**
+ * Returns object where fn is applied to just the values.
+ * @param {object} obj
+ * @param {function(val): val} fn
+ * @returns {object}
+ **/
+export function mapVals(obj, fn) {
+    return Object.keys(obj).reduce((acc, cur) => {
+        const val = fn(obj[cur]);
+        return {...acc, [cur]: val};
+    }, {});
+}
+
+/**
+ * Returns object filtered by applying the pred to just the keys.
+ * Note, number keys will be sent to fn as a string.
+ * @param {object} obj
+ * @param {function([key, val]): boolean)} pred
+ * @returns {object}
+ **/
+export function filterObj(obj, pred) {
+    return Object.keys(obj).reduce((acc, cur) => (
+        pred([cur, obj[cur]])
+            ? {...acc, [cur]: obj[cur]}
+            : acc
+    ), {});
+}
+
+/**
+ * Returns object filtered by applying the pred to just the keys.
+ * Note, number keys will be sent to fn as a string.
+ * @param {object} obj
+ * @param {function(key): boolean} pred
+ * @returns {object}
+ **/
+export function filterKeys(obj, pred) {
+    return Object.keys(obj).reduce((acc, cur) => (
+        pred(cur) ? {...acc, [cur]: obj[cur]} : acc
+    ), {});
+}
+
+/**
+ * Returns object filtered by applying the pred to just the values.
+ * @param {object} obj
+ * @param {function(val): boolean} pred
+ * @returns {object}
+ **/
+export function filterVals(obj, pred) {
+    return Object.keys(obj).reduce((acc, cur) => (
+        pred(obj[cur]) ? {...acc, [cur]: obj[cur]} : acc
+    ), {});
+}
+
+/**
+ * Returns key of value that matches pred.
+ * @param {object} obj
+ * @param {function(val): boolean} pred
+ * @returns {key}
+ **/
+export function findObjKey(obj, pred) {
+    return Object.keys(obj).find(key => pred(obj[key]));
+}
+
+/**
+ * Returns the value that matches pred.
+ * @param {object} obj
+ * @param {function(val): boolean} pred
+ * @returns {val}
+ **/
+export function findObjVal(obj, pred) {
+    const foundKey = Object.keys(obj).find(key => pred(obj[key]));
+    return foundKey ? obj[foundKey] : undefined;
+}
