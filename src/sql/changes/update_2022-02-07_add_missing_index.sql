4b65ea129e3562ffabd9316037a15e35eb851c05
Matt Spencer <sirmspencer@gmail.com>
2022-04-18 17:23:59 -0700

    remove old change files

--- src/sql/changes/update_2022-02-07_add_missing_index.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX projects_institution_rid    ON projects (institution_rid);
-CREATE INDEX project_imagery_project_rid ON project_imagery (project_rid);
-CREATE INDEX ext_samples_plot_rid        ON ext_samples (plot_rid);
-CREATE INDEX plot_assignments_plot_rid   ON plot_assignments (plot_rid);
-CREATE INDEX plot_assignments_user_rid   ON plot_assignments (user_rid);
--- src/sql/changes/update_2022-02-10_allow_shuffle_plots.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE projects ADD COLUMN shuffle_plots boolean;
