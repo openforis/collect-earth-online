7d8dc3e1e68a84358d6aebf52cf3269e45adee2f
Sif Biri <sbiri@sig-gis.com>
2022-05-11 12:31:30 -0700

    Add nREPL and CIDER support

---
+++ .dir-locals.el
@@ -0,0 +1,1 @@
+((nil . ((cider-clojure-cli-global-options . "-J--add-modules=jdk.incubator.foreign -J--enable-native-access=ALL-UNNAMED"))))
--- src/clj/collect_earth_online/server.clj
+++ src/clj/collect_earth_online/server.clj
@@ -1,7 +1,8 @@
 (ns collect-earth-online.server
   (:require [clojure.java.io     :as io]
             [clojure.string      :as str]
-            [clojure.core.server :refer [start-server]]
+            [nrepl.server :as nrepl-server]
+            [cider.nrepl :refer (cider-nrepl-handler)]
             [ring.adapter.jetty :refer [run-jetty]]
             [triangulum.cli     :refer [get-cli-options]]
             [triangulum.config  :refer [get-config]]
@@ -78,8 +79,8 @@
                "  Create an SSL key for HTTPS or run without the --https-port (-P) option.")
       (do
         (when repl
-          (println "Starting REPL server on port 5555")
-          (reset! repl-server (start-server {:name :ceo-repl :port 5555 :accept 'clojure.core.server/repl})))
+          (println "Starting nREPL server on port 5555")
+          (reset! repl-server (nrepl-server/start-server :port 5555 :handler cider-nrepl-handler)))
         (reset! server (run-jetty handler config))
         (reset! clean-up-service (start-clean-up-service!))
         (set-log-path! log-dir)
