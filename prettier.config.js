61336d13e9003f76fc32e6cf379e5c4b08af39ca
Sif Biri <sbiri@sig-gis.com>
2022-08-02 10:54:30 -0700

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //  These settings are duplicated in .editorconfig:
+  tabWidth: 2, // indent_size = 2
+  useTabs: false, // indent_style = space
+  endOfLine: "lf", // end_of_line = lf
+  semi: true, // default: true
+  singleQuote: false, // default: false
+  printWidth: 100, // default: 80
+  trailingComma: "es5",
+  bracketSpacing: true,
+};
