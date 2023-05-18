52faeb7d1cd2db9d8f43891724dcb5da8795414c
a-luz <arthurg.luz@gmail.com>
2023-05-18 10:32:02 -0300

    deleting sql change for creating views

--- src/sql/changes/2023-04-29-create-plots-and-samples-views.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW plot_shapes AS
-SELECT plot_geom AS geometry, project_rid as project_id
-FROM plots;
-
-CREATE VIEW sample_shapes AS
-SELECT s.sample_geom AS geometry, p.project_rid as project_id, s.plot_rid as plot_id
-FROM samples AS s
-INNER JOIN plots AS p ON p.plot_uid = s.plot_rid;
