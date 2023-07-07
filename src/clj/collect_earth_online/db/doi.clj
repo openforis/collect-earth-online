6cdd98d63680e5a7086d0cadc83cb3289ae85789
a-luz <arthurg.luz@gmail.com>
2023-07-07 10:47:10 -0300

    Fixing require

--- src/clj/collect_earth_online/db/doi.clj
+++ src/clj/collect_earth_online/db/doi.clj
@@ -4,7 +4,7 @@
     [clojure.data.json :as json]
     [clojure.java.io :as io]
     [collect-earth-online.generators.external-file :refer [create-and-zip-files-for-doi]]
-    [collect-earth-online.views :refer [data-response redirect]]
+    [collect-earth-online.views :refer [data-response]]
     [triangulum.config  :refer [get-config]]
     [triangulum.database :refer [call-sql]]
     [triangulum.type-conversion :as tc])
