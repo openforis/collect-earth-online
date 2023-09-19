73386292510407e7bc30f78d37cb91a0434027bb
Andrew Carlile <acarlile@sig-gis.com>
2023-09-19 11:03:50 -0400

    adds documentation to new ns

--- src/js/utils/kml.js
+++ src/js/utils/kml.js
@@ -1,3 +1,17 @@
+/*****************************************************************************
+ ***
+ *** KML Support 
+ ***
+ *****************************************************************************/
+
+
+/*
+  fillNode: creates a <Style> node to inject into a KML string.
+  Parameters: (kml: string, fill: string)
+    kml is a string parsed from kml.
+    fill is a string representing a number that represents the 'fill' value of the generated style node. defaults to 0.
+  Return: stylenode: KML Node
+*/
 const fillNode = (kml, fill) => {
     const styleNode = kml.createElement('Style');
     const polyStyleNode = kml.createElement('PolyStyle');
@@ -7,7 +21,14 @@ const fillNode = (kml, fill) => {
     styleNode.appendChild(polyStyleNode);
     return styleNode;
   };
-  
+
+/*
+  outlineKML: Injects a Style Node into supplied KML.
+  Parameters: (KMLString: ol.format.KML)
+  Return: ol.format.KML
+  !! NOT PURE !!
+  mutates supplied KML object
+*/
 export function  outlineKML (KMLString) {
     const Parser = new DOMParser();
     const Serializer = new XMLSerializer();
