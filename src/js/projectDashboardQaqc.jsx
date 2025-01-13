88e8a9a9b49e65cd78089f958b8a7ca335be96f8
a-luz <aluz@sig-gis.com>
2025-01-13 13:30:30 -0300

    Fix arg types in plots functions

--- src/js/projectDashboardQaqc.jsx
+++ src/js/projectDashboardQaqc.jsx
@@ -13,7 +13,7 @@ import { projectConditionalRowStyles,
 
 import { mercator } from "./utils/mercator";
 
-class ProjectDashboard extends React.Component {
+class ProjectDashboardQaqc extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -635,7 +635,7 @@ const Tab = ({ label, children }) => {
 export function pageInit(params, session) {
   ReactDOM.render(
     <NavigationBar userId={session.userId} userName={session.userName} version={session.versionDeployed}>
-      <ProjectDashboard projectId={params.projectId || "0"} userName={session.userName} />
+      <ProjectDashboardQaqc projectId={params.projectId || "0"} userName={session.userName} />
     </NavigationBar>,
     document.getElementById("app")
   );
