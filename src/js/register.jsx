a16df7f28eb79be0d3515f26070932d68d27eddd
Andrew Carlile <acarlile@sig-gis.com>
2025-05-23 12:44:37 -0400

    conditionally render UI for registration

--- src/js/register.jsx
+++ src/js/register.jsx
@@ -12,10 +12,14 @@ class Register extends React.Component {
       passwordConfirmation: "",
       acceptTOS: false,
       acceptDataTOS: false,
-      modal: null
+      modal: null,
+      userId: null
     };
   }
-
+  fetchUserIfExists = () => {
+    console.log(this.state.email);
+    this.setState ({userId: 1});
+  };
   register = () => {
     if (!this.state.acceptTOS || !this.state.acceptDataTOS) {
       this.setState({modal: {alert: {alertType: "Registration Alert",
@@ -68,64 +72,75 @@ class Register extends React.Component {
                   className="form-control"
                   id="email"
                   onChange={(e) => this.setState({ email: e.target.value })}
+                  onBlur={() => this.fetchUserIfExists()}
                   placeholder="Email"
                   type="email"
                   value={this.state.email}
                 />
               </div>
-              <div className="form-group">
-                <label htmlFor="password">Enter your password</label>
-                <input
-                  autoComplete="off"
-                  className="form-control"
-                  id="password"
-                  onChange={(e) => this.setState({ password: e.target.value })}
-                  placeholder="Password"
-                  type="password"
-                  value={this.state.password}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password-confirmation">Confirm your password</label>
-                <input
-                  autoComplete="off"
-                  className="form-control"
-                  id="password-confirmation"
-                  onChange={(e) => this.setState({ passwordConfirmation: e.target.value })}
-                  placeholder="Password confirmation"
-                  type="password"
-                  value={this.state.passwordConfirmation}
-                />
-              </div>
-              <div className="form-check">
-                <input
-                  checked={this.state.acceptTOS}
-                  className="form-check-input"
-                  id="tos-check"
-                  onChange={() => this.setState({ acceptTOS: !this.state.acceptTOS })}
-                  type="checkbox"
-                />
-                <label className="form-check-label" htmlFor="tos-check">
-                  I agree to the{" "}
-                  <a href="/terms-of-service" target="_blank">
-                    Terms of Service
-                  </a>
-                  .
-                </label>
-              </div>
-              <div className="form-check mb-3">
-                <input
-                  checked={this.state.acceptDataTOS}
-                  className="form-check-input"
-                  id="data-tos-check"
-                  onChange={() => this.setState({ acceptDataTOS: !this.state.acceptDataTOS })}
-                  type="checkbox"
-                />
-              </div>
-              <button className="btn btn-lightgreen float-right mb-2" type="submit">
-                Register
-              </button>
-            </form>
+              {this.state.userId === 0 && 
+               <React.Fragment>
+                 <div className="form-group">
+                   <label htmlFor="password">Enter your password</label>
+                   <input
+                     autoComplete="off"
+                     className="form-control"
+                     id="password"
+                     onChange={(e) => this.setState({ password: e.target.value })}
+                     placeholder="Password"
+                     type="password"
+                     value={this.state.password}
+                   />
+                 </div>
+                 
+                 <div className="form-group">
+                   <label htmlFor="password-confirmation">Confirm your password</label>
+                   <input
+                     autoComplete="off"
+                     className="form-control"
+                     id="password-confirmation"
+                     onChange={(e) => this.setState({ passwordConfirmation: e.target.value })}
+                     placeholder="Password confirmation"
+                     type="password"
+                     value={this.state.passwordConfirmation}
+                   />
+                 </div>
+                 <div className="form-check">
+                   <input
+                     checked={this.state.acceptTOS}
+                     className="form-check-input"
+                     id="tos-check"
+                     onChange={() => this.setState({ acceptTOS: !this.state.acceptTOS })}
+                     type="checkbox"
+                   />
+                   <label className="form-check-label" htmlFor="tos-check">
+                     I agree to the{" "}
+                     <a href="/terms-of-service" target="_blank">
+                       Terms of Service
+                     </a>
+                     .
+                   </label>
+                 </div>
+                 <div className="form-check mb-3">
+                   <input
+                     checked={this.state.acceptDataTOS}
+                     className="form-check-input"
+                     id="data-tos-check"
+                     onChange={() => this.setState({ acceptDataTOS: !this.state.acceptDataTOS })}
+                     type="checkbox"
+                   />
+                 </div>
+               </React.Fragment>}
+              
+              <button className="btn btn-lightgreen float-right mb-2"
+                       type="submit"
+                       disabled={!this.state.userId}>
+                 { this.state.userId && this.state.userId > 0 ? "Resend Validation Email" : "Register"}
+		 </button>
+              
+              
+              
+              </form>
           </div>
         </div>
       </div>
