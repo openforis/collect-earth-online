144953c8adbcdf26c19b7fbe7c3accab593187ac
Andrew Carlile <acarlile@sig-gis.com>
2025-07-03 16:37:12 -0400

    tests include schema and sample data

--- test/collect_earth_online/core_test.clj
+++ test/collect_earth_online/core_test.clj
@@ -1,6 +1,21 @@
 (ns collect-earth-online.core-test
-  (:require [clojure.test :refer [is deftest testing]]))
+  (:require [clojure.edn     :as edn]
+            [clojure.test    :refer [is deftest testing]]
+            [malli.core      :as m]))
 
-(deftest ^:unit workflow-test
-  (testing "A simple test"
+(deftest ^:unit dummy-test
+  (testing "This test fails"
+    (is false))
+  (testing "This test passes"
     (is (= 2 (+ 1 1)))))
+
+(deftest ^:unit sample-data-test
+  (testing "Test data conforms to spec"
+    (let [user (edn/read-string (slurp "test/data/sample_user.edn"))
+          user-spec [:map
+                     [:user_id :int]
+                     [:email :string]
+                     [:administrator :boolean]
+                     [:reset_key [:maybe :string]]]
+          valid? (m/validate user-spec user)]      
+      (is valid?))))
