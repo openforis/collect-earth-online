d3135dcbcf117ec7d8efaaa8937e0452c8de7522
Andrew Carlile <acarlile@sig-gis.com>
2025-07-22 17:06:42 -0400

    expands sample test

--- test/collect_earth_online/db/projects_test.clj
+++ test/collect_earth_online/db/projects_test.clj
@@ -1,69 +1,81 @@
 (ns collect-earth-online.db.projects
   (:require [clojure.test :refer [is deftest testing]]
-            [collect-earth-online.db.projects :as projects]))
+            [clojure.test.check.generators :as [gen]]
+            [collect-earth-online.db.projects :as projects]
+            [triangulum.database :refer [call-sql sql-primitive]]))
 
-(comment
-  
-  (def mock-data {:user_id 0 :email "admin@ceo.dev"})
-  (deftest pure-function-test
-    (is (= 0 0)))
-  (deftest side-effect-test
-    (with-redefs [db/query (constantly mock-data)]
-      (is (= 0 0))))
-  )
+(deftest ^:unit can-collect?-test
+  (testing "Happy path: A user that is allowed to collect for given project"
+    (is (projects/can-collect? 1 1 "")))
+  (testing "A user that is not allowed to collect for given project")
+  (testing "Cannot find user")
+  (testing "Cannot find project"))
 
-(deftest ^:unit can-collect?)
+(deftest ^:unit is-proj-admin?-test
+  (testing "Happy path: Given user is an admin for given project")
+  (testing "Given user is not an admin for given project")
+  (testing "User cannot be found in db")
+  (testing "Project cannot be found in db"))
 
-(deftest ^:unit is-proj-admin?)
+(deftest ^:unit get-home-projects-test
+  (testing "Happy Path: finds projects"
+    (with-redefs [call-sql (fn [])]
+      (let [response (projects/get-home-projects "happy-path-params")]
+        (is (= 200 (:status response))
+            (vector? (:body response))))))
+  (testing "Empty Result case"
+    (with-redefs [call-sql (constantly [])]
+      (is (= {:status 200 :body []}
+             (projects/get-home-projects "empty-result-params")))))
+  (testing "Invalid params throw"
+    (is (thrown? Exception (projects/get-home-projects "invalid-params")))))
 
-(deftest ^:unit get-home-projects)
+(deftest ^:unit get-institution-projects-test)
 
-(deftest ^:unit get-institution-projects)
+(deftest ^:unit get-institution-dash-projects-test)
 
-(deftest ^:unit get-institution-dash-projects)
+(deftest ^:unit get-template-projects-test)
 
-(deftest ^:unit get-template-projects)
+(deftest ^:unit get-project-by-id-test)
 
-(deftest ^:unit get-project-by-id)
+(deftest ^:unit get-template-by-id-test)
 
-(deftest ^:unit get-template-by-id)
-
-(deftest ^:unit get-project-stats)
+(deftest ^:unit get-project-stats-test)
 n
-(deftest ^:unit create-project!)
+(deftest ^:unit create-project!-test)
 
-(deftest ^:unit reset-collected-samples!)
+(deftest ^:unit reset-collected-samples!-test)
 
-(deftest ^:unit update-project!)
+(deftest ^:unit update-project!-test)
 
-(deftest ^:unit publish-project!)
+(deftest ^:unit publish-project!-test)
 
-(deftest ^:unit close-project!)
+(deftest ^:unit close-project!-test)
 
-(deftest ^:unit archive-project!)
+(deftest ^:unit archive-project!-test)
 
-(deftest ^:unit plots->csv-response)
+(deftest ^:unit plots->csv-response-test)
 
-(deftest ^:unit dump-project-aggregate-data!)
+(deftest ^:unit dump-project-aggregate-data!-test)
 
-(deftest ^:unit dump-project-raw-data!)
+(deftest ^:unit dump-project-raw-data!-test)
 
-(deftest ^:unit create-shape-files!)
+(deftest ^:unit create-shape-files!-test)
 
-(deftest ^:unit check-plot-csv)
+(deftest ^:unit check-plot-csv-test)
 
-(deftest ^:unit get-project-drafts-by-user)
+(deftest ^:unit get-project-drafts-by-user-test)
 
-(deftest ^:unit get-project-draft-by-id)
+(deftest ^:unit get-project-draft-by-id-test)
 
-(deftest ^:unit create-project-draft!)
+(deftest ^:unit create-project-draft!-test)
 
-(deftest ^:unit update-project-draft!)
+(deftest ^:unit update-project-draft!-test)
 
-(deftest ^:unit delete-project-draft!)
+(deftest ^:unit delete-project-draft!-test)
 
-(deftest ^:unit edit-projects-bulk!)
+(deftest ^:unit edit-projects-bulk!-test)
 
-(deftest ^:unit edit-projects-bulk!)
+(deftest ^:unit edit-projects-bulk!-test)
 
-(deftest ^:unit download-projects-bulk!)
+(deftest ^:unit download-projects-bulk!-test)
