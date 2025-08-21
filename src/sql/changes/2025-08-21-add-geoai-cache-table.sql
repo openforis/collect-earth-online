9cbb01bbfd8d7ca7cab0a9dc4167ed5b1a6b5721
a-luz <aluz@sig-gis.com>
2025-08-21 11:06:44 -0300

    Creating new geoai caching table

---
+++ src/sql/changes/2025-08-21-add-geoai-cache-table.sql
@@ -0,0 +1,9 @@
+UPDATE TABLE projects ADD COLUMN plot_similatirty_asset text;
+
+CREATE TABLE geoai_cache (
+  geoai_cache_uid SERIAL,
+  project_rid integer NOT NULL REFERENCES projects(project_uid) ON DELETE CASCADE ON UPDATE CASCADE,
+  plot_rid integer NOT NULL REFERENCES plots(plot_uid) ON DELETE CASCADE ON UPDATE CASCADE,
+  similar_plots jsonb,
+  metadata jsonb
+);
