a30bd7f93b0ed63e6cc4ec4f3856971dcd8ced56
Arthur G Luz <aluz@sig-gis.com>
2025-08-29 10:07:08 -0300

    Update 2025-08-21-add-geoai-cache-table.sql
    Changing similar_plots type from jsonb to integer[]

--- src/sql/changes/2025-08-21-add-geoai-cache-table.sql
+++ src/sql/changes/2025-08-21-add-geoai-cache-table.sql
@@ -4,6 +4,6 @@ CREATE TABLE geoai_cache (
   geoai_cache_uid SERIAL,
   project_rid integer NOT NULL REFERENCES projects(project_uid) ON DELETE CASCADE ON UPDATE CASCADE,
   plot_rid integer NOT NULL REFERENCES plots(plot_uid) ON DELETE CASCADE ON UPDATE CASCADE,
-  similar_plots jsonb,
+  similar_plots integer[],
   metadata jsonb
 );
