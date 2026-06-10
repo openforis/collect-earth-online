16ae73aa3afe4e349b1a141cd5c29d6508a0cb3e
Andrew <andrew.carlil3@gmail.com>
2026-06-10 13:21:06 -0400

    naively adds validation to projectwizard

--- src/js/projectWizard.jsx
+++ src/js/projectWizard.jsx
@@ -26,6 +26,8 @@ import {
   sub_ids
 } from "./state/projectWizard";
 
+import { lengthObject, someObject, filterObject } from "./utils/sequence";
+
 import "../css/project-wizard.css";
 
 
@@ -35,7 +37,7 @@ const wizardModalAtom = atom(null);
 const projectSteps = [
   {id: 'overview', label: 'Project Overview'},
   {id: 'imagery', label: 'Imagery Selection'},
-  {id: 'boundary', label: 'Project Boudary'},
+  {id: 'boundary', label: 'Project Boundary'},
   {id: 'plots', label: 'Plot Generation'},
   {id: 'samples', label: 'Sample Design'},
   {id: 'questions', label: 'Survey Questions'},
@@ -57,13 +59,202 @@ const errorModal = {
   }
 };
 
+function validateOverview ({name, description}) {
+  return(['overview', [
+    (name === "" || description === "") && "A project must contain a name and description.",
+  ]]);
+}
+
+function validateImagery ({requiresPublic, imageryId, privacyLevel}) {
+  return(['imagery', [
+    requiresPublic &&
+      `Projects with privacy level of ${privacyLevel} require at least one public imagery.`,
+      imageryId <= 0 && "Select a valid Basemap.",
+  ]]);
+}
+
+function validatePlots({
+  plotDistribution,
+  aoiFeatures,
+  plotSpacing,
+  plotSize,
+  plotFileNeeded,
+  plotFileName,
+  totalPlots,
+  plotLimit,
+  users,
+  userMethod,
+  percents,
+  qaqcMethod,
+  smes,
+  timesToReview,
+  numPlots
+  
+}) {
+  return (['plots',
+          [
+  ["random", "gridded"].includes(plotDistribution) &&
+        !aoiFeatures.length &&
+        "Please select a valid boundary.",
+      plotDistribution === "random" &&
+        !numPlots &&
+        "A number of plots is required for random plot distribution.",
+      plotDistribution === "gridded" &&
+        !plotSpacing &&
+        "A plot spacing is required for gridded plot distribution.",
+    !["shp", "geojson"].includes(plotDistribution) && (!plotSize || plotSize === 0) && "A plot size is required.",
+      plotDistribution === "csv" &&
+        plotFileNeeded &&
+        !(plotFileName || "").includes(".csv") &&
+        "A plot CSV (.csv) file is required.",
+      plotDistribution === "shp" &&
+        plotFileNeeded &&
+        !(plotFileName || "").includes(".zip") &&
+        "A plot SHP (.zip) file is required.",
+      totalPlots > plotLimit &&
+        "The plot size limit has been exceeded. Check the Plot Design section for detailed info.",
+      ["equal", "percent"].includes(userMethod) &&
+        users.length === 0 &&
+        "At least one user must be added to the plot assignment.",
+      userMethod === "percent" &&
+        percents.reduce((acc, p) => acc + p, 0) !== 100 &&
+        "The assigned plot percentages must equal 100%.",
+      userMethod === "percent" &&
+        percents.reduce((acc, p) => acc || p === 0, false) &&
+        "All plot assignment percentages must be greater than 0%.",
+      ["overlap", "sme"].includes(qaqcMethod) &&
+        percent === 0 &&
+        "The assigned Quality Control percentage must be greater than 0%.",
+      ["random", "gridded"].includes(plotDistribution) &&
+      qaqcMethod === "sme" && smes.length === 0 && "At least one user must be added as an SME.",
+      qaqcMethod === "overlap" &&
+        users.length === 1 &&
+        "At least two assigned users are required for overlap mode.",
+      qaqcMethod === "overlap" && timesToReview < 2 && "# of Reviews must be at least 2.",
+      qaqcMethod === "overlap" &&
+        timesToReview > users.length &&
+        users.length > 1 &&
+        "# of Reviews cannot be greater than the number of assigned users.",
+      userMethod !== "none" &&
+        qaqcMethod === "sme" &&
+        _.intersection(users, smes).length > 0 &&
+        "Users cannot be an Assigned User and an SME. Please remove the duplicate users.",]]);
+}
+
+function validateSamples({
+  sampleDistribution,
+  samplesPerPlot,
+  sampleResolution,
+  sampleFileName,
+  plotShape,
+  plotSize,
+  perPlotLimit,
+  totalPlots,
+  sampleLimit,
+  allowDrawnSamples,
+  sampleGeometries
+}) {
+  return ([
+    'samples',
+    [sampleDistribution === "random" &&
+     !samplesPerPlot && "A number of samples per plot is required for random sample distribution.",
+     sampleDistribution === "gridded" &&
+     !sampleResolution && "A sample spacing is required for gridded sample distribution.",
+     sampleDistribution === "csv" &&
+     !(sampleFileName || "").includes(".csv") && "A sample CSV (.csv) file is required.",
+     sampleDistribution === "shp" &&
+     !(sampleFileName || "").includes(".zip") && "A sample SHP (.zip) file is required.",
+     sampleDistribution === "gridded" &&
+     plotShape === "circle" &&
+     sampleResolution >= plotSize / Math.sqrt(2) && `You must use a sample spacing that is less than ${
+          Math.round((plotSize / Math.sqrt(2)) * 100) / 100
+        } meters.`,
+     sampleDistribution === "gridded" &&
+     plotShape === "square" &&
+     sampleResolution >= plotSize && "The sample spacing must be less than the plot width.",
+     (samplesPerPlot > perPlotLimit || totalPlots * samplesPerPlot > sampleLimit) &&
+     "The sample size limit has been exceeded. Check the Sample Design section for detailed info.",
+     allowDrawnSamples && !Object.values(sampleGeometries).some((g) => g) &&
+     "At least one geometry type must be enabled.",]]);
+}
+
+function validateQuestions({
+  surveyQuestions
+}) {
+  function allAnswersHidden (answers) {
+    const hiddenAnswers = filterObject(answers, ([_id, ans]) => ans.hide);
+    return lengthObject(answers) === lengthObject(hiddenAnswers);}
+
+  return ([
+    'questions',
+    [lengthObject(surveyQuestions) === 0 && "A survey must include at least one question.",
+     someObject(surveyQuestions, ([_id, sq]) => (lengthObject(sq.answers) === 0 || allAnswersHidden(sq.answers))) &&
+     "All survey questions must contain at least one (unhidden) answer.",]
+  ]);
+}
+
+function validateWizard () {
+  /* validates the existing draft project.
+     returns a boolean.
+     side effects: sends api request to create project,
+     or adds errors to state.
+     may even send a request, if superficially valid, and still add errors to state, in case of server error
+  */
+  const form = {name : "",
+	        description : "",
+
+	        privacyLevel : "",
+	        imageryId : "",
+	        institutionImagery : "",
+	        projectImageryList : "",
+	        requiresPublic : " ",
+
+	        projectId : -1,
+	        aoiFeatures : [],
+	        plotDistribution : "",
+	        numPlots : -1,
+	        plotSpacing: "",
+	        plotSize : -1,
+	        plotFileName: "",
+	        useTemplatePlots : false,
+//	 originalProject ,
+	 designSettings: {
+	   
+	 },
+	        totalPlots : -1,
+	        plotFileNeeded : false,
+	        allowDrawnSamples : false,
+	        samplesPerPlot : -1,
+	 
+	        plotShape : "",
+	        sampleDistribution : "",
+	        sampleFileName : "",
+	        sampleResolution : "",
+
+	        surveyQuestions : []};
+
+  const errors = [
+    validateOverview(form),
+    validateImagery(form),
+    validatePlots(form),
+    validateSamples(form),
+    validateQuestions(form)
+  ].filter(([name, e])=>e.length);
+  
+  return (errors.length ? false : errors);
+}
+
 const publishModal = {
   title: 'Ready to publish this project?',
   id: 'review',
   closeText: "Cancel",
   confirmText: "Publish Project",
   onConfirm: ()=>{
-    dispatch([event_ids.modal, successModal]);
+    let errors = validateWizard();
+    errors ?
+      dispatch ([event_ids.modal, errorModal]) //dispatch an event that adds errors to state and then sends errors
+      : dispatch([event_ids.modal, successModal]); //dispatch an event that sends the api request and eventually succesModal
+      
   }
 };
 
