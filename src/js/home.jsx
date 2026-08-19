7bf849c2ed14923acccce8e5c935d2790a382039
Andrew <andrew.carlil3@gmail.com>
2026-08-19 11:12:20 -0400

    default headers for future tabs

--- src/js/home.jsx
+++ src/js/home.jsx
@@ -566,6 +566,33 @@ function HomeTabs ({tab, session}) {
   switch (tab) {
   case 'highlights':
     return (<Highlights userId={session.userId} userRole={session.userRole}/>);
+  case 'institutions' :
+    return (<div id='institutions-tab' className='home-tab'>
+              <div className="header">
+                <div className="header-row">
+                  <p className="header-title">Institutions</p>
+                  <p className="header-subtitle"></p>
+                </div>
+              </div>
+            </div>);
+  case 'collect' :
+    return (<div id='collect-tab' className='home-tab'>
+              <div className="header">
+                <div className="header-row">
+                  <p className="header-title">Collect</p>
+                  <p className="header-subtitle"></p>
+                </div>
+              </div>
+            </div>);
+    case 'manage' :
+    return (<div id='manage-tab' className='home-tab'>
+              <div className="header">
+                <div className="header-row">
+                  <p className="header-title">Manage</p>
+                  <p className="header-subtitle"></p>
+                </div>
+              </div>
+            </div>);
     
   default: return (<></>);
   }
